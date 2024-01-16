3c85a90d8348f47e284fed62c47f07b4df257b13
n0rrman <email@henriknorrman.com>
2024-01-16 17:58:29 +0700

    fylo-data-storage-component init

---
+++ fylo-data-storage-component/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Fylo data storage component",
+  description:
+    "This component has some interesting CSS challenges in the design. If you're looking to test your CSS skills, this will be a great project for you!",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ fylo-data-storage-component/src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      <div>You’ve used 815 GB of your storage</div>
+
+      <div>185 GB Left</div>
+      <div>0 GB</div>
+      <div>1000 GB</div>
+    </div>
+  );
+}
