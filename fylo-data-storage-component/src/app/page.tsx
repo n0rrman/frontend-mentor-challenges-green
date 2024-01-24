2f3d028e8b692a952e921608a106566dc4cea857
n0rrman <email@henriknorrman.com>
2024-01-24 23:16:32 +0700

    fylo-data-storage-component mobile layout done

--- fylo-data-storage-component/src/app/page.tsx
+++ fylo-data-storage-component/src/app/page.tsx
@@ -6,6 +6,8 @@ import logo from "/public/logo.svg";
 import documentIcon from "/public/icon-document.svg";
 import folderIcon from "/public/icon-folder.svg";
 import uploadIcon from "/public/icon-upload.svg";
+import bgMobile from "/public/bg-mobile.png";
+import bgDesktop from "/public/bg-desktop.png";
 
 export default function Home() {
   const min_storage = 0;
@@ -15,7 +17,17 @@ export default function Home() {
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-veryDarkBlue">
-      <main className="flex flex-row gap-5">
+      <div className="absolute bottom-0 w-full h-[100vh] sm:h-[50vh] z-0">
+        <picture className="w-auto h-auto">
+          <source media="(min-width: 60rem)" srcSet={bgDesktop.src} />
+          <img
+            className="w-[100vw] h-[100vh] sm:h-[50vh] object-cover z-0"
+            src={bgMobile.src}
+            alt=""
+          />
+        </picture>
+      </div>
+      <main className="flex flex-col sm:flex-row gap-5 z-10">
         <div className="flex flex-col bg-darkBlue rounded-xl pr-20 rounded-tr-[5rem] gap-5 p-8">
           <div className="relative w-[8.4375rem] h-[2.5rem]">
             <Image src={logo} alt="" fill />
@@ -38,8 +50,8 @@ export default function Home() {
             </button>
           </div>
         </div>
-        <div className="flex flex-col self-end items-end">
-          <div className="bg-white w-fit p-3.5 rounded-xl -mb-6 z-10">
+        <div className="flex flex-col-reverse sm:flex-col self-end items-center sm:items-end">
+          <div className="bg-white w-fit p-3.5 rounded-xl -mt-6 -mb-0 sm:mt-0 sm:-mb-6 z-10">
             <div className="">
               <span className="text-veryDarkBlue text-3xl font-bold pr-2">
                 {remaining_storage}
