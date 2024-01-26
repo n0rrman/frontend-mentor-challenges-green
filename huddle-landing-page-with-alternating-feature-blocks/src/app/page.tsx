3f52be7a61e9e4c2aea48aaafd03de5e9b0c6be9
n0rrman <email@henriknorrman.com>
2024-01-26 23:47:37 +0700

    huddle-landing-page-with-alternating-feature-blocks semantic html completed

--- huddle-landing-page-with-alternating-feature-blocks/src/app/page.tsx
+++ huddle-landing-page-with-alternating-feature-blocks/src/app/page.tsx
@@ -19,45 +19,64 @@ export default function Home() {
         <div className="w-1/2">IMAGE</div>
       </section>
 
-      <div>Grow Together</div>
-      <div>
-        Generate meaningful discussions with your audience and build a strong,
-        loyal community. Think of the insightful conversations you miss out on
-        with a feedback form.
-      </div>
+      <section className="px-20 py-16">
+        <div className="flex flex-row">
+          <div className="w-1/2">
+            <h2>Grow Together</h2>
+            <p>
+              Generate meaningful discussions with your audience and build a
+              strong, loyal community. Think of the insightful conversations you
+              miss out on with a feedback form.
+            </p>
+          </div>
+          <div className="w-1/2">IMAGE</div>
+        </div>
 
-      <div>Flowing Conversations</div>
+        <div className="flex flex-row">
+          <div className="w-1/2">
+            <h2>Flowing Conversations</h2>
+            <p>
+              You wouldn't paginate a conversation in real life, so why do it
+              online? Our threads have just-in-time loading for a more natural
+              flow.
+            </p>
+          </div>
+          <div className="w-1/2">IMAGE</div>
+        </div>
 
-      <div>
-        You wouldn't paginate a conversation in real life, so why do it online?
-        Our threads have just-in-time loading for a more natural flow.
-      </div>
+        <div className="flex flex-row">
+          <div className="w-1/2">
+            <h2>Your Users</h2>
+            <p>
+              It takes no time at all to integrate Huddle with your app's
+              authentication solution. This means, once signed in to your app,
+              your users can start chatting immediately.
+            </p>
+          </div>
+          <div className="w-1/2">IMAGE</div>
+        </div>
+      </section>
 
-      <div>Your Users</div>
+      <footer>
+        <div>
+          <h2>Ready To Build Your Community?</h2>
+          <button>Get Started For Free</button>
+        </div>
 
-      <div>
-        It takes no time at all to integrate Huddle with your app's
-        authentication solution. This means, once signed in to your app, your
-        users can start chatting immediately.
-      </div>
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua
+        </div>
 
-      <div>Ready To Build Your Community?</div>
+        <div>+1-543-123-4567</div>
+        <div>example@huddle.com</div>
 
-      <div>Get Started For Free</div>
+        <div>About Us What We Do FAQ</div>
 
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua
-      </div>
+        <div>Career Blog Contact Us</div>
 
-      <div>+1-543-123-4567</div>
-      <div>example@huddle.com</div>
-
-      <div>About Us What We Do FAQ</div>
-
-      <div>Career Blog Contact Us</div>
-
-      <div>&copy; Copyright 2018 Huddle. All rights reserved.</div>
+        <div>&copy; Copyright 2018 Huddle. All rights reserved.</div>
+      </footer>
     </div>
   );
 }
