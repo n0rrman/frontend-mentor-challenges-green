74db42ea58d6abbe3a96831311f7e2176d946374
n0rrman <email@henriknorrman.com>
2024-01-31 23:50:59 +0700

    advice-generator-app started

--- advice-generator-app/src/app/page.tsx
+++ advice-generator-app/src/app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <div> Advice</div>;
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-darkBlue">
+      <main className="text-[1.75rem] w-96 bg-darkGrayishBlue text-white">
+        Advice
+      </main>
+    </div>
+  );
 }
