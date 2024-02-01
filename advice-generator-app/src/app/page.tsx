59e4464f9d29b38c5c98e344ddbc5da3b5083d89
n0rrman <email@henriknorrman.com>
2024-02-01 23:57:14 +0700

    advice-generator-app layout done

--- advice-generator-app/src/app/page.tsx
+++ advice-generator-app/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex justify-center items-center min-h-screen bg-darkBlue">
-      <main className="text-[1.75rem] w-96 bg-darkGrayishBlue text-white">
+      <main className="flex justify-center items-center text-[1.75rem] w-[33.75rem] h-[20.75rem] rounded-2xl shadow-xl bg-darkGrayishBlue text-white">
         Advice
       </main>
     </div>
