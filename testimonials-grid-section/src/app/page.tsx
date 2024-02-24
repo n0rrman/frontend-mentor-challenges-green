bb3f27d26aabad6207599b00db90057c53a3d9eb
n0rrman <email@henriknorrman.com>
2024-02-24 11:27:10 +0700

    testimonials-grid-section layout + functionality finished

--- testimonials-grid-section/src/app/page.tsx
+++ testimonials-grid-section/src/app/page.tsx
@@ -8,65 +8,68 @@ import patrickPic from "/public/image-patrick.jpg";
 
 export default function Home() {
   return (
-    <main className="grid grid-cols-4 grid-rows-2 order-1 gap-8 justify-center min-h-screen">
-      <Card
-        className="col-span-2 bg-moderateViolet"
-        name="Daniel Clifford"
-        title="Verified Graduate"
-        profilePic={danielPic}
-        heading="I received a job offer mid-course, and the subjects I learned were current, if not more so, in the company I joined. I honestly feel I got every penny's worth."
-        text="I was an EMT for many years before I joined the bootcamp. I've been
-          looking to make a transition and have heard some people who had an
-          amazing experience here. I signed up for the free intro course and found
-          it incredibly fun! I enrolled shortly thereafter. The next 12 weeks was
-          the best - and most grueling - time of my life. Since completing the
-          course, I've successfully switched careers, working as a Software
-          Engineer at a VR startup."
-      />
+    <main className="flex justify-center items-center min-h-screen bg-lightGrayishBlue font-medium text-white">
+      <section className="flex flex-col mx-6 sm:grid grid-cols-2 lg:grid-cols-4 gap-x-[1.875rem] gap-y-6 container lg:max-w-[69.375rem]">
+        <Card
+          className="col-span-2 bg-moderateViolet"
+          name="Daniel Clifford"
+          title="Verified Graduate"
+          profilePic={danielPic}
+          first
+          heading="I received a job offer mid-course, and the subjects I learned were current, if not more so, in the company I joined. I honestly feel I got every penny's worth."
+          text="I was an EMT for many years before I joined the bootcamp. I've been
+        looking to make a transition and have heard some people who had an
+        amazing experience here. I signed up for the free intro course and found
+        it incredibly fun! I enrolled shortly thereafter. The next 12 weeks was
+        the best - and most grueling - time of my life. Since completing the
+        course, I've successfully switched careers, working as a Software
+        Engineer at a VR startup."
+        />
 
-      <Card
-        className="order-2 bg-veryDarkGrayishBlue"
-        name="Jonathan Walters"
-        title="Verified Graduate"
-        profilePic={jonathanPic}
-        heading="The team was very supportive and kept me motivated"
-        text="I started as a total newbie with virtually no coding skills. I now
+        <Card
+          className="order-2 bg-veryDarkGrayishBlue"
+          name="Jonathan Walters"
+          title="Verified Graduate"
+          profilePic={jonathanPic}
+          heading="The team was very supportive and kept me motivated"
+          text="I started as a total newbie with virtually no coding skills. I now
         work as a mobile engineer for a big company. This was one of the best
         investments I've made in myself."
-      />
+        />
 
-      <Card
-        className="order-4 bg-white"
-        name="Jeanette Harmon"
-        title="Verified Graduate"
-        profilePic={jeanettePic}
-        heading="An overall wonderful and rewarding experience"
-        text="Thank you for the wonderful experience! I now have a job I really
+        <Card
+          className="order-4 bg-white text-veryDarkBlackishBlue"
+          name="Jeanette Harmon"
+          title="Verified Graduate"
+          profilePic={jeanettePic}
+          heading="An overall wonderful and rewarding experience"
+          text="Thank you for the wonderful experience! I now have a job I really
         enjoy, and make a good living while doing something I love."
-      />
+        />
 
-      <Card
-        className="col-span-2 order-5 bg-veryDarkBlackishBlue"
-        name="Patrick Abrams"
-        title="Verified Graduate"
-        profilePic={patrickPic}
-        heading="Awesome teaching support from TAs who did the bootcamp themselves.
+        <Card
+          className="col-span-2 order-5 bg-veryDarkBlackishBlue"
+          name="Patrick Abrams"
+          title="Verified Graduate"
+          profilePic={patrickPic}
+          purpleBorder
+          heading="Awesome teaching support from TAs who did the bootcamp themselves.
         Getting guidance from them and learning from their experiences was easy."
-        text="The staff seem genuinely concerned about my progress which I find
+          text="The staff seem genuinely concerned about my progress which I find
         really refreshing. The program gave me the confidence necessary to be
         able to go out in the world and present myself as a capable junior
         developer. The standard is above the rest. You will get the personal
         attention you need from an incredible community of smart and amazing
         people."
-      />
+        />
 
-      <Card
-        className="row-span-2 order-3 bg-white"
-        name="Kira Whittle"
-        title="Verified Graduate"
-        profilePic={kiraPic}
-        heading="Such a life-changing experience. Highly recommended!"
-        text="Before joining the bootcamp, I've never written a line of code. I
+        <Card
+          className="row-span-2 order-last sm:order-3 bg-white text-veryDarkBlackishBlue"
+          name="Kira Whittle"
+          title="Verified Graduate"
+          profilePic={kiraPic}
+          heading="Such a life-changing experience. Highly recommended!"
+          text="Before joining the bootcamp, I've never written a line of code. I
         needed some structure from professionals who can help me learn
         programming step by step. I was encouraged to enroll by a former student
         of theirs who can only say wonderful things about the program. The
@@ -77,7 +80,8 @@ export default function Home() {
         during interviews as an example of my developent experience. It
         certainly helped me land a job as a full-stack developer after receiving
         multiple offers. 100% recommend!"
-      />
+        />
+      </section>
     </main>
   );
 }
--- testimonials-grid-section/src/components/card.tsx
+++ testimonials-grid-section/src/components/card.tsx
@@ -1,11 +1,15 @@
 import Image, { StaticImageData } from "next/image";
 
+import bgIcon from "/public/bg-pattern-quotation.svg";
+
 interface CardProps {
   name: string;
   title: string;
   profilePic: StaticImageData;
   heading: string;
   text: string;
+  purpleBorder?: boolean;
+  first?: boolean;
   className: string;
 }
 
@@ -15,21 +19,34 @@ export default function Card({
   profilePic,
   heading,
   text,
+  purpleBorder,
+  first,
   className,
 }: CardProps) {
   return (
-    <div className={`flex flex-col ${className}`}>
+    <div
+      className={`relative flex flex-col ${className} shadow-2xl rounded-lg py-5 px-8 gap-3 text-[0.8125rem]`}
+    >
       <div className="flex flex-row justify-start items-center">
-        <div className="relative h-7 w-7 rounded-full overflow-hidden">
+        {first && (
+          <div className="absolute right-[10%] top-0 z-10">
+            <Image src={bgIcon} alt="" width={100} height={100} />
+          </div>
+        )}
+        <div
+          className={`relative h-8 w-8 rounded-full overflow-hidden mr-5 border-2 ${
+            purpleBorder ? "border-moderateViolet" : "border-white/30"
+          }`}
+        >
           <Image alt="" src={profilePic} fill />
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col z-30">
           <h2>{name}</h2>
-          <h3 className="opacity-50">{title}</h3>
+          <h3 className="opacity-50 font-bold">{title}</h3>
         </div>
       </div>
-      <h1>{heading}</h1>
-      <p className="opacity-70 text-[0.8125rem]">{text}</p>
+      <h1 className="font-semibold text-[1.25rem] leading-6 z-30">{heading}</h1>
+      <p className="opacity-70 leading-[1.1rem] z-30">" {text} "</p>
     </div>
   );
 }
