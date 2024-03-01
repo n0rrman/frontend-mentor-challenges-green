09d0323bd99b8053cabbdd79fb6a4508c1a49685
n0rrman <email@henriknorrman.com>
2024-03-01 23:55:13 +0700

    clipboard-landing-page init

---
+++ clipboard-landing-page/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Clipboard landing page",
+  description:
+    "Put your layout skills to the test with this HTML & CSS landing page challenge. This challenge includes a design for hover states.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
