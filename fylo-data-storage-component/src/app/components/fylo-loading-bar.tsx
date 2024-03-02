a6937d51dbf398b8668bf73ebc93287db3b4eca6
n0rrman <email@henriknorrman.com>
2024-03-02 23:07:32 +0700

    fylo-data-storage-component done

--- fylo-data-storage-component/src/app/components/fylo-loading-bar.tsx
+++ fylo-data-storage-component/src/app/components/fylo-loading-bar.tsx
@@ -6,12 +6,14 @@ interface FyloLoadingBarProps {
   min: number;
   max: number;
   current: number;
+  className: string;
 }
 
 export default function FyloLoadingBar({
   min,
   max,
   current,
+  className,
 }: FyloLoadingBarProps) {
   const [percentage, setPercentage] = useState(0);
 
@@ -19,10 +21,14 @@ export default function FyloLoadingBar({
     setPercentage(Math.trunc((current / (max - min)) * 100));
   }, [min, max, current]);
 
+  console.log(percentage);
+
   return (
-    <div className="flex justify-start w-full bg-veryDarkBlue p-1 rounded-2xl">
+    <div
+      className={`flex justify-start w-full bg-veryDarkBlue/50 p-0.5 rounded-2xl ${className}`}
+    >
       <div
-        className={`flex justify-end bg-gradient-to-r from-gradientFrom to-gradientTo h-4 p-0.5 transition-all rounded-2xl`}
+        className={`flex justify-end bg-gradient-to-r from-gradientFrom to-gradientTo h-3.5 p-0.5 transition-all rounded-2xl`}
         style={{ width: `${percentage}%` }}
       >
         <div className="rounded-full h-full aspect-square bg-white"></div>
--- fylo-data-storage-component/src/app/page.tsx
+++ fylo-data-storage-component/src/app/page.tsx
@@ -16,62 +16,67 @@ export default function Home() {
   const remaining_storage = max_storage - used_storage;
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-veryDarkBlue">
-      <div className="absolute bottom-0 w-full h-[100vh] sm:h-[50vh] z-0">
+    <div className="flex justify-center items-center min-h-screen px-6">
+      <div className="absolute bottom-0 w-full h-screen z-0">
         <picture className="w-auto h-auto">
           <source media="(min-width: 60rem)" srcSet={bgDesktop.src} />
           <img
-            className="w-[100vw] h-[100vh] sm:h-[50vh] object-cover z-0"
+            className=" absolute bottom-0 w-[100vw] h-[1/2] object-cover"
             src={bgMobile.src}
             alt=""
           />
         </picture>
       </div>
-      <main className="flex flex-col sm:flex-row gap-5 z-10">
-        <div className="flex flex-col bg-darkBlue rounded-xl pr-20 rounded-tr-[5rem] gap-5 p-8">
+      <main className="flex flex-col w-full items-center justify-center md:flex-row gap-4 sm:gap-[1.875rem] z-10 translate-y-0 sm:-translate-y-2.5">
+        <div className="flex flex-col justify-center items-start p-10 bg-darkBlue shadow-2xl rounded-lg rounded-tr-[6.4em] gap-[2.05rem] max-w-[21.875rem] w-full">
           <div className="relative w-[8.4375rem] h-[2.5rem]">
             <Image src={logo} alt="" fill />
           </div>
-          <div className="flex flex-row gap-3">
+          <div className="flex flex-row gap-4">
             <button className="bg-veryDarkBlue rounded-lg p-3 w-12 h-12">
-              <div className="relative w-full h-full">
+              <div className="mx-auto relative h-full aspect-[0.833]">
                 <Image src={documentIcon} alt="" fill />
               </div>
             </button>
             <button className="bg-veryDarkBlue rounded-lg p-3 w-12 h-12">
-              <div className="relative w-full h-full">
+              <div className="relative w-full aspect-[1.2]">
                 <Image src={folderIcon} alt="" fill />
               </div>
             </button>
             <button className="bg-veryDarkBlue rounded-lg p-3 w-12 h-12">
-              <div className="relative w-full h-full">
+              <div className="relative w-full aspect-[1.5]">
                 <Image src={uploadIcon} alt="" fill />
               </div>
             </button>
           </div>
         </div>
-        <div className="flex flex-col-reverse sm:flex-col self-end items-center sm:items-end">
-          <div className="bg-white w-fit p-3.5 rounded-xl -mt-6 -mb-0 sm:mt-0 sm:-mb-6 z-10">
-            <div className="">
-              <span className="text-veryDarkBlue text-3xl font-bold pr-2">
+        <div className="flex flex-col-reverse md:flex-col self-end items-center sm:items-end w-full max-w-[33.75rem]">
+          <div className="relative bg-white w-fit py-1.5 px-6 rounded-xl -mt-6 -mb-0 md:mt-0 sm:-mb-6 z-20 mr-0 md:mr-10">
+            <div className="flex flex-row justify-center items-center">
+              <span className="text-veryDarkBlue text-[2.5rem] font-bold pr-2">
                 {remaining_storage}
               </span>
-              <span className="text-grayishBlue text-xs uppercase font-bold">
+              <span className="text-grayishBlue tracking-widest text-xs uppercase font-bold z-20">
                 GB Left
               </span>
             </div>
-            <div className=""></div>
+            <div className="hidden md:block absolute right-0 bg-white z-10 h-10 w-10 skew-y-[45deg] bottom-0"></div>
           </div>
-          <div className="p-8 bg-darkBlue text-white rounded-xl">
-            <div>You've used {used_storage} GB of your storage</div>
+          <div className="flex flex-col gap-0.5 p-10 bg-darkBlue rounded-xl w-full shadow-xl text-xs">
+            <div className="pb-2 text-[0.875rem] text-paleBlue/70">
+              You&apos;ve used{" "}
+              <span className="text-paleBlue">{used_storage} GB</span> of your
+              storage
+            </div>
 
             <FyloLoadingBar
+              className="my-1.5"
               min={min_storage}
               max={max_storage}
               current={used_storage}
             />
 
-            <div className="flex flex-row justify-between items-center font-bold">
+            <div className="flex flex-row justify-between items-center text-paleBlue">
               <div>{min_storage} GB</div>
               <div>{max_storage} GB</div>
             </div>
