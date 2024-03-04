2d856d82c7c7e2394e97b590005935ee9e213a8b
n0rrman <email@henriknorrman.com>
2024-03-04 15:01:32 +0700

    clipboard-landing-page all text content added

--- clipboard-landing-page/src/app/page.tsx
+++ clipboard-landing-page/src/app/page.tsx
@@ -1,5 +1,84 @@
 export default function Home() {
-    return (
-      <div>Home Page</div>  
-    );
-  }
+  return (
+    <>
+      <main>
+        <section>
+          <h1>A history of everything you copy</h1>
+
+          <p>
+            Clipboard allows you to track and organize everything you copy.
+            Instantly access your clipboard on all your devices.
+          </p>
+
+          <button>Download for iOS</button>
+          <button>Download for Mac</button>
+        </section>
+
+        <section>
+          <h1>Keep track of your snippets</h1>
+          <p>
+            Clipboard instantly stores any item you copy in the cloud, meaning
+            you can access your snippets immediately on all your devices. Our
+            Mac and iOS apps will help you organize everything.
+          </p>
+          <h2>Quick Search</h2>
+          <p>
+            Easily search your snippets by content, category, web address,
+            application, and more.
+          </p>
+          <h2>iCloud Sync</h2>
+          <p>Instantly saves and syncs snippets across all your devices.</p>
+          <h2>Complete History</h2>
+          <p>
+            Retrieve any snippets from the first moment you started using the
+            app.
+          </p>
+        </section>
+
+        <section>
+          <h2>Access Clipboard anywhere</h2>
+          <p>
+            Whether you&apos;re on the go, or at your computer, you can access
+            all your Clipboard snippets in a few simple clicks.
+          </p>
+        </section>
+        <section>
+          <h1>Supercharge your workflow</h1>
+          <p>We&apos;ve got the tools to boost your productivity.</p>
+          <h2>Create blacklists</h2>
+          <p>
+            Ensure sensitive information never makes its way to your clipboard
+            by excluding certain sources.
+          </p>
+          <h2>Plain text snippets</h2>
+
+          <p>
+            Remove unwanted formatting from copied text for a consistent look.
+          </p>
+          <h2>Sneak preview</h2>
+          <p>
+            Quick preview of all snippets on your Clipboard for easy access.
+          </p>
+        </section>
+        <section>
+          <h1>Clipboard for iOS and Mac OS</h1>
+
+          <p>
+            Available for free on the App Store. Download for Mac or iOS, sync
+            with iCloud and you&apos;re ready to start adding to your clipboard.
+          </p>
+
+          <button>Download for iOS</button>
+          <button>Download for Mac</button>
+        </section>
+      </main>
+      <footer>
+        <div>FAQs</div>
+        <div>Contact Us</div>
+        <div>Privacy Policy</div>
+        <div>Press Kit</div>
+        <div>Install Guide</div>
+      </footer>
+    </>
+  );
+}
