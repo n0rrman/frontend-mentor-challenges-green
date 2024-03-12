a51b6da4db74d04efc4c7ca7a0f9047a462bb817
n0rrman <email@henriknorrman.com>
2024-03-12 23:35:53 +0700

    fylo-landing-page-with-two-column-layout init

---
+++ fylo-landing-page-with-two-column-layout/README.md
@@ -0,0 +1,22 @@
+# Frontend Mentor - Fylo landing page with two column layout solution
+
+This is a solution to the [Fylo landing page with two column layout challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/fylo-landing-page-with-two-column-layout-5ca5ef041e82137ec91a50f5). 
+
+
+
+### Screenshot
+
+![Screenshot of the completed challenge](./design/screenshot.png)
+
+
+### Links
+
+- Solution URL: [Solution at Frontend Mentor](...)
+- Live Site URL: [Live version on Vercel](...)
+
+
+### Built with
+
+- [Next.js](https://nextjs.org/) - JavaScript framework
+- [Tailwind CSS](https://tailwindcss.com/) - CSS framework
+
---
+++ fylo-landing-page-with-two-column-layout/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Fylo landing page with two column layout",
+  description:
+    "This project is great if you're starting to get confident with slightly more complex layouts. The second section with the testimonial area will provide a nice challenge.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
