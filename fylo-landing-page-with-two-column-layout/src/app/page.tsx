c0e97389f3dace4d4e04dacb69313951115b206f
n0rrman <email@henriknorrman.com>
2024-03-14 22:37:31 +0700

    fylo-landing-page-with-two-column-layout all text content added

--- fylo-landing-page-with-two-column-layout/src/app/page.tsx
+++ fylo-landing-page-with-two-column-layout/src/app/page.tsx
@@ -1,5 +1,71 @@
 export default function Home() {
-    return (
-      <div>Home Page</div>  
-    );
-  }
+  return (
+    <>
+      <header>
+        <div>Features</div>
+        <div>Team</div>
+        <div>Sign In</div>
+      </header>
+      <main>
+        <section>
+          <h1>All your files in one secure location, accessible anywhere.</h1>
+          <p>
+            Fylo stores your most important files in one secure location. Access
+            them wherever you need, share and collaborate with friends, family,
+            and co-workers.
+          </p>
+
+          <button>Get Started</button>
+        </section>
+        <section>
+          <h1>Stay productive, wherever you are</h1>
+
+          <p>
+            Never let location be an issue when accessing your files. Fylo has
+            you covered for all of your file storage needs.
+          </p>
+          <p>
+            Securely share files and folders with friends, family and colleagues
+            for live collaboration. No email attachments required!
+          </p>
+
+          <div>See how Fylo works</div>
+          <div>
+            Fylo has improved our team productivity by an order of magnitude.
+            Since making the switch our team has become a well-oiled
+            collaboration machine.
+          </div>
+
+          <div>
+            <div>Kyle Burton</div>
+            <div>Founder & CEO, Huddle</div>
+          </div>
+        </section>
+
+        <section>
+          <h1>Get early access today</h1>
+          <p>
+            It only takes a minute to sign up and our free starter tier is
+            extremely generous. If you have any questions, our support team
+            would be happy to help you.
+          </p>
+
+          <button>Get Started For Free</button>
+        </section>
+
+        <footer>
+          <div>Phone: +1-543-123-4567</div>
+          <div>example@fylo.com</div>
+          <div>About Us</div>
+          <div>Jobs</div>
+          <div>Press</div>
+          <div>Blog</div>
+
+          <div>Contact Us</div>
+          <div>Terms</div>
+          <div>Privacy</div>
+        </footer>
+      </main>
+    </>
+  );
+}
