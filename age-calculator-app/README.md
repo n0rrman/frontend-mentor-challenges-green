82dcc0c8793a896b402632bed6b4e6b1a31a56e5
n0rrman <email@henriknorrman.com>
2024-03-21 23:51:43 +0700

    age-calculator-app init

---
+++ age-calculator-app/README.md
@@ -0,0 +1,19 @@
+# Frontend Mentor - Age calculator app solution
+
+This is a solution to the [Age calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/age-calculator-app-dF9DFFpj-Q).
+
+### Screenshot
+
+![Screenshot of the completed challenge](./design/screenshot.png)
+
+
+### Links
+
+- Solution URL: [Solution at Frontend Mentor](...)
+- Live Site URL: [Live version on Vercel](...)
+
+
+### Built with
+
+- [Next.js](https://nextjs.org/) - JavaScript framework
+- [Tailwind CSS](https://tailwindcss.com/) - CSS framework
---
+++ age-calculator-app/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Age calculator app",
+  description:
+    "This challenge is designed to sharpen your JavaScript and form validation skills. Working with dates in JavaScript can be tricky, so this will be a nice test!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
