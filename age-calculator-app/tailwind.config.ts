6df030f5fa72172bf6521fd0cb355ee49b98bca5
n0rrman <email@henriknorrman.com>
2024-03-24 19:42:47 +0700

    age-calculator-app colors and fonts added

--- age-calculator-app/tailwind.config.ts
+++ age-calculator-app/tailwind.config.ts
@@ -6,7 +6,20 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    extend:{
+      colors: {
+        purple: 'hsl(var(--color-purple) / <alpha-value>)',
+        lightRed: 'hsl(var(--color-lightRed) / <alpha-value>)',
+
+        white: 'hsl(var(--color-white) / <alpha-value>)',
+        offWhite: 'hsl(var(--color-offWhite) / <alpha-value>)',
+        lightGray: 'hsl(var(--color-lightGray) / <alpha-value>)',
+        smokeyGray: 'hsl(var(--color-smokeyGray) / <alpha-value>)',
+        offBlack: 'hsl(var(--color-offBlack) / <alpha-value>)',
+      }
+    }
+  },
   plugins: [],
 }
 export default config
