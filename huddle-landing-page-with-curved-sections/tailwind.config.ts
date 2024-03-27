b92be2b409f1d81ed02eca0a283e3c95de0e1a3f
n0rrman <email@henriknorrman.com>
2024-03-27 23:48:18 +0700

    huddle-landing-page-with-curved-sections colours added

--- huddle-landing-page-with-curved-sections/tailwind.config.ts
+++ huddle-landing-page-with-curved-sections/tailwind.config.ts
@@ -6,7 +6,18 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        pink: 'hsl(var(--color-pink) / <alpha-value>)',
+        lightPink: 'hsl(var(--color-lightPink) / <alpha-value>)',
+        lightRed: 'hsl(var(--color-lightRed) / <alpha-value>)',
+
+        veryDarkCyan: 'hsl(var(--color-veryDarkCyan) / <alpha-value>)',
+        veryDarkBlue: 'hsl(var(--color-veryDarkBlue) / <alpha-value>)',
+      }
+    }
+  },
   plugins: [],
 }
 export default config
