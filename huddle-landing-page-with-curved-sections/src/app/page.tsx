f4068d8573336b329ecc564ea8a0b503712c6f71
n0rrman <email@henriknorrman.com>
2024-03-28 23:48:34 +0700

    huddle-landing-page-with-curved-sections text content added

--- huddle-landing-page-with-curved-sections/src/app/page.tsx
+++ huddle-landing-page-with-curved-sections/src/app/page.tsx
@@ -1,5 +1,61 @@
 export default function Home() {
-    return (
-      <div>Home Page</div>  
-    );
-  }
+  return (
+    <div>
+      <div>Try it free</div>
+      <div>Build The Community Your Fans Will Love</div>
+      <div>
+        Huddle re-imagines the way we build communities. You have a voice, but
+        so does your audience. Create connections with your users as you engage
+        in genuine discussion.
+      </div>
+
+      <div>Get Started For Free</div>
+
+      <div>1.4k+</div>
+      <div>Communities Formed</div>
+
+      <div>2.7m+</div>
+      <div>Messages Sent</div>
+
+      <div>Grow Together</div>
+
+      <div>
+        Generate meaningful discussions with your audience and build a strong,
+        loyal community. Think of the insightful conversations you miss out on
+        with a feedback form.
+      </div>
+
+      <div>Flowing Conversations</div>
+      <div>
+        You wouldn't paginate a conversation in real life, so why do it online?
+        Our threads have just-in-time loading for a more natural flow.
+      </div>
+
+      <div>Your Users</div>
+      <div>
+        It takes no time at all to integrate Huddle with your app's
+        authentication solution. This means, once signed in to your app, your
+        users can start chatting immediately.
+      </div>
+
+      <div>Ready To Build Your Community?</div>
+      <div>Get Started For Free</div>
+
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nulla
+        quam, hendrerit lacinia vestibulum a, ultrices quis sem.
+      </div>
+
+      <div>Phone: +1-543-123-4567</div>
+      <div>example@huddle.com</div>
+
+      <div>Newsletter</div>
+      <div>
+        To recieve tips on how to grow your community, sign up to our weekly
+        newsletter. We'll never send you spam or pass on your email address
+      </div>
+
+      <div>Subscribe</div>
+    </div>
+  );
+}
