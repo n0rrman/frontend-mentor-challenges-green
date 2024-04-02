5c0fd3bd155ff6a7d004bd6209c36c2760939ea8
n0rrman <email@henriknorrman.com>
2024-04-02 23:57:21 +0700

    age-calculator-app form checks added

--- age-calculator-app/src/actions/index.ts
+++ age-calculator-app/src/actions/index.ts
@@ -2,11 +2,21 @@
 
 import { z } from "zod";
 
+
 const submitCalcSchema = z.object({
-    days: z.number(),
-    months: z.number(),
-    years: z.number()
+    days: z.number().gte(1, { message: "too small" }).lte(31, { message: "too big"}),
+    months: z.number().gte(1, { message: "too small"}).lte(12, { message: "too big"}),
+    years: z.number().gte(0, { message: "too small"}).lte(new Date().getFullYear(), { message: "in future"})
 })
+
+/*
+- Any field is empty when the form is submitted
+- The day number is not between 1-31
+- The month number is not between 1-12
+- The date is in the future
+- The date is invalid e.g. 31/04/1991 (there are 30 days in April)
+- **Bonus**: See the age numbers animate to their final number when the form is submitted
+*/
 
 interface SubmitCalcFormState {
     errors: {
@@ -14,22 +24,20 @@ interface SubmitCalcFormState {
         months?: string[];
         years?: string[];
     }
-    succes?: boolean;
+    success?: boolean;
 }
 
 export async function submitCalcForm(formState: SubmitCalcFormState, formData: FormData): Promise<SubmitCalcFormState> {
 
     const results = submitCalcSchema.safeParse({
-        days: formData.get('days'),
-        months: formData.get('months'),
-        years: formData.get('years'),
+        days: Number(formData.get('days') || ""),
+        months: Number(formData.get('months') || ""),
+        years: Number(formData.get('years') || ""),
     })
 
-    console.log(formData.get('days'))
-    console.log(formData.get('months'))
-    console.log(formData.get('years'))
-
+    
     if(!results.success) {
+        console.log(results.error.flatten().fieldErrors)
         return {
             errors: results.error.flatten().fieldErrors
         }
@@ -37,6 +45,6 @@ export async function submitCalcForm(formState: SubmitCalcFormState, formData: F
 
     return {
         errors: {},
-        succes: true,
+        success: true,
     }
 }
