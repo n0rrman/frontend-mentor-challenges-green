d06d52a2c3c71b423a84cfa6c5759b9946e0ff74
n0rrman <email@henriknorrman.com>
2024-04-05 23:42:26 +0700

    tip-calculator-app init

---
+++ tip-calculator-app/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Tip calculator app",
+  description:
+    "This small app is perfect for anyone starting to get to grips with JavaScript. The calculator functionality will be a nice test!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
