8af001ee50758e51c118b42bd508c8bef1fdce2f
n0rrman <email@henriknorrman.com>
2024-04-06 21:59:30 +0700

    tip-calculator-app text content added

--- tip-calculator-app/src/app/page.tsx
+++ tip-calculator-app/src/app/page.tsx
@@ -1,5 +1,26 @@
 export default function Home() {
-    return (
-      <div>Home Page</div>  
-    );
-  }
+  return (
+    <div>
+      <div>
+        <div>Bill</div>
+        <div>Select Tip %</div>
+
+        <div>5%</div>
+        <div>10%</div>
+        <div>15%</div>
+        <div>25%</div>
+        <div>50%</div>
+        <div>Custom</div>
+        <div>Number of People</div>
+      </div>
+      <div>
+        <div>Tip Amount</div>
+        <div>/ person</div>
+        <div>Total</div>
+        <div>/ person</div>
+
+        <div>Reset</div>
+      </div>
+    </div>
+  );
+}
