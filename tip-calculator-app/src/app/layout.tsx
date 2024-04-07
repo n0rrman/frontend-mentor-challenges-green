7884fdd471bb2cbe40b9d983a1a8eaac94b9c92e
n0rrman <email@henriknorrman.com>
2024-04-07 17:21:42 +0700

    tip-calculator-app colours and fonts added

--- tip-calculator-app/src/app/layout.tsx
+++ tip-calculator-app/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Space_Mono } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const space = Space_Mono({ subsets: ["latin"], weight: ["700"] });
 
 export const metadata: Metadata = {
   title: "Tip calculator app",
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={space.className}>{children}</body>
     </html>
   );
 }
--- tip-calculator-app/tailwind.config.ts
+++ tip-calculator-app/tailwind.config.ts
@@ -6,7 +6,20 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        strongCyan: 'hsl(var(--color-strongCyan) / <alpha-value>)',
+        
+        darkCyan: 'hsl(var(--color-darkCyan) / <alpha-value>)',
+        darkGrayishCyan: 'hsl(var(--color-darkGrayishCyan) / <alpha-value>)',
+        grayishCyan: 'hsl(var(--color-grayishCyan) / <alpha-value>)',
+        lightGrayishCyan: 'hsl(var(--color-lightGrayishCyan) / <alpha-value>)',
+        lighterGrayishCyan: 'hsl(var(--color-lighterGrayishCyan) / <alpha-value>)',
+        white: 'hsl(var(--color-white) / <alpha-value>)',
+      }
+    }
+  },
   plugins: [],
 }
 export default config
