001b961b2113ccf5e6183a2110554bf404f5e151
n0rrman <email@henriknorrman.com>
2024-04-18 22:53:16 +0700

    expenses-chart-component dynamic weekly view added

--- expenses-chart-component/src/components/weekly-view.tsx
+++ expenses-chart-component/src/components/weekly-view.tsx
@@ -18,12 +18,21 @@ export default function WeeklyView({ spendings }: WeeklyViewProps) {
   const renderedBars = spendings.map((spending) => {
     return (
       <div
-        className="flex flex-col justify-end items-center py-6"
+        className="flex flex-col-reverse gap-2 justify-start items-center py-6"
         id={spending.day}
       >
-        <div>{spending.amount}</div>
-        <div style={{ height: (150 * spending.amount) / max }}>x</div>
-        <div>{spending.day}</div>
+        <div className="text-mediumBrown">{spending.day}</div>
+        <div
+          className={`${
+            max == spending.amount
+              ? "bg-cyan hover:bg-cyan/70"
+              : "bg-softRed hover:bg-softRed/70"
+          } w-12 rounded transition-all peer`}
+          style={{ height: (150 * spending.amount) / max }}
+        ></div>
+        <div className="bg-darkBrown opacity-0 peer-hover:opacity-100 text-veryPaleOrange p-2 rounded-lg text-[1.125rem] transition-all">
+          ${spending.amount}
+        </div>
       </div>
     );
   });
