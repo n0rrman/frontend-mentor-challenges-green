9df40fa79646516699db996a0b321cd9670978f5
n0rrman <email@henriknorrman.com>
2024-04-23 23:53:27 +0700

    notifications-page init

---
+++ notifications-page/README.md
@@ -0,0 +1,20 @@
+# Frontend Mentor - Notifications page solution
+
+This is a solution to the [Notifications page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/notifications-page-DqK5QAmKbC). 
+
+
+### Screenshot
+
+![Screenshot of the completed challenge](./design/screenshot.png)
+
+
+### Links
+
+- Solution URL: [Solution at Frontend Mentor](...)
+- Live Site URL: [Live version on Vercel](...)
+
+
+### Built with
+
+- [Next.js](https://nextjs.org/) - JavaScript framework
+- [Tailwind CSS](https://tailwindcss.com/) - CSS framework
---
+++ notifications-page/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Notifications page",
+  description:
+    "This project will be a brilliant test of your HTML, CSS, and basic JavaScript skills. You'll use JS to toggle the visual state of the notifications.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
