a0089a3b4a2e97ed38cc92ec90865ce46cadfa26
n0rrman <email@henriknorrman.com>
2024-04-24 23:49:32 +0700

    clipboard-landing-page done

--- clipboard-landing-page/src/app/page.tsx
+++ clipboard-landing-page/src/app/page.tsx
@@ -49,25 +49,25 @@ export default function Home() {
           </div>
         </section>
 
-        <section className="flex flex-col justify-center items-center z-20 gap-4 py-1.5">
-          <h1 className="text-[2.175rem] font-semibold text-darkGrayishBlue">
+        <section className="flex flex-col justify-center items-center z-20 gap-4 py-1.5 px-8 sm:px-0">
+          <h1 className="text-[1.7rem] sm:text-[2.175rem] font-semibold text-darkGrayishBlue">
             Keep track of your snippets
           </h1>
-          <p className="max-w-[45rem] leading-7 text-[1.15rem]">
+          <p className="max-w-[45rem] leading-7 text-[1rem] sm:text-[1.15rem]">
             Clipboard instantly stores any item you copy in the cloud, meaning
             you can access your snippets immediately on all your devices. Our
             Mac and iOS apps will help you organize everything.
           </p>
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-12 mt-[4.25rem]">
-            <div className="relative w-10  md:w-[47rem] aspect-[1.3146853147] overflow-hidden">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 mt-10 sm:mt-[4.25rem]">
+            <div className="relative w-full lg:w-[47rem] aspect-[1.3146853147] overflow-hidden">
               <Image
-                className="translate-x-0 md:-translate-x-8 2xl:translate-x-0"
+                className="translate-x-0 lg:-translate-x-8 2xl:translate-x-0"
                 src={compImg}
                 fill
                 alt=""
               />
             </div>
-            <div className="flex flex-col gap-y-[3.6rem] text-[1rem] md:text-left pl-0 md:pl-[5.4rem] pt-[4.6rem] text-center">
+            <div className="flex flex-col gap-y-[3.6rem] text-[1rem] lg:text-left pl-0 lg:pl-[5.4rem] pt-4 lg:pt-[4.6rem] text-center">
               <div className="space-y-3">
                 <h2 className="text-[1.45rem] leading-6 font-semibold text-darkGrayishBlue">
                   Quick Search
@@ -98,29 +98,29 @@ export default function Home() {
           </div>
         </section>
 
-        <section className="flex flex-col justify-center items-center z-20 gap-4 pt-[3.9rem]">
-          <h1 className="text-[2.175rem] font-semibold text-darkGrayishBlue">
+        <section className="flex flex-col justify-center items-center z-20 gap-4 pt-[3.9rem] px-7 sm:px-0">
+          <h1 className="text-[1.7rem] sm:text-[2.175rem] font-semibold text-darkGrayishBlue">
             Access Clipboard anywhere
           </h1>
-          <p className="max-w-[45rem] text-[1.15rem] leading-[1.85rem] mb-[5.25rem]">
+          <p className="max-w-[45rem] text-[1rem] sm:text-[1.15rem] leading-[1.85rem] mb-[5.25rem]">
             Whether you&apos;re on the go, or at your computer, you can access
             all your Clipboard snippets in a few simple clicks.
           </p>
-          <div className="flex justify-center items-center relative w-full h-20 md:w-[56.5rem] md:h-[36rem] translate-x-0.5">
+          <div className="flex justify-center items-center relative w-[95vw] -m-10 aspect-[1.6] md:w-[56.5rem] md:h-[36rem] translate-x-0.5">
             <Image src={tabletImg} fill alt="" />
           </div>
         </section>
 
-        <section className="flex flex-col justify-center items-center z-20 gap-4">
-          <h1 className="text-[2.175rem] font-semibold text-darkGrayishBlue -mt-3">
+        <section className="flex flex-col justify-center items-center z-20 gap-4 px-7 sm:px-0">
+          <h1 className="text-[1.7rem] sm:text-[2.175rem] font-semibold text-darkGrayishBlue -mt-3">
             Supercharge your workflow
           </h1>
 
-          <p className="text-[1.15rem]">
+          <p className="text-[1rem] sm:text-[1.15rem]">
             We&apos;ve got the tools to boost your productivity.
           </p>
 
-          <div className="grid grid-cols-1 md:grid-cols-3 items-start gap-6 pt-9 mt-0.5 pb-[8.6rem] px-[10rem] text-[0.97rem] leading-[1.9rem]">
+          <div className="grid grid-cols-1 md:grid-cols-3 items-start gap-6 pt-9 mt-0.5 pb-[8.6rem] px-2 sm:px-[10rem] text-[0.97rem] leading-[1.9rem]">
             <div className="flex flex-col gap-4 items-center justify-center">
               <div className="relative w-11 h-10 m-5">
                 <Image src={blackWorklist} fill alt="" />
@@ -160,7 +160,7 @@ export default function Home() {
               </p>
             </div>
           </div>
-          <div className="flex flex-col md:flex-row gap-[6rem] justify-center items-center pt-1 pb-[4.6rem]">
+          <div className="flex flex-col flex-wrap md:flex-row gap-[4rem] sm:gap-[6rem] justify-center items-center pt-1 pb-[4.6rem] px-4">
             <div className="relative w-[165px] h-[49px]">
               <Image src={googleLogo} fill alt="" />
             </div>
@@ -178,17 +178,17 @@ export default function Home() {
             </div>
           </div>
         </section>
-        <section className="flex flex-col justify-center items-center z-20 gap-4 max-w-[45rem] pb-8">
-          <h1 className="text-[2.2rem] font-semibold text-darkGrayishBlue">
+        <section className="flex flex-col justify-center items-center z-20 gap-4 max-w-[45rem] pb-8 px-7 sm:px-0">
+          <h1 className="text-[1.7rem] sm:text-[2.2rem] font-semibold text-darkGrayishBlue">
             Clipboard for iOS and Mac OS
           </h1>
 
-          <p>
+          <p className="text-[1rem] sm:text-inherit">
             Available for free on the App Store. Download for Mac or iOS, sync
             with iCloud and you&apos;re ready to start adding to your clipboard.
           </p>
 
-          <div className="flex flex-col md:flex-row text-white/90 font-semibold gap-4 pt-9 pb-10">
+          <div className="flex flex-col md:flex-row text-white/80 font-semibold gap-4 py-10 w-full sm:w-max">
             <button className="bg-strongCyan hover:bg-opacity-80 transition-all shadow-xl shadow-strongCyan/20 tracking-wide rounded-[2rem] py-[0.8rem] px-[2.2rem] border border-b-[3px] border-black/10">
               Download for iOS
             </button>
@@ -198,25 +198,35 @@ export default function Home() {
           </div>
         </section>
       </main>
-      <footer className="flex flex-col md:flex-row gap-[8.5rem] text-darkGrayishBlue/80 text-[1.1rem] items-center justify-center bg-grayishBlue/10 py-10 px-0 md:px-[10.3rem] mt-12">
-        <div className="-translate-y-1">
-          <Image width={54} height={54} alt="logo" src={logo} />
+      <footer className="flex flex-col md:flex-row gap-12 sm:gap-[8.5rem] text-darkGrayishBlue/80 text-[1.1rem] items-center justify-center bg-grayishBlue/10 py-10 px-0 md:px-[10.3rem] mt-12">
+        <div className="relative -translate-y-1 w-[3.375rem] aspect-square flex-shrink-0">
+          <Image fill alt="logo" src={logo} />
         </div>
-        <div className="grid gap-x-[4.8rem] text-left gap-y-4 grid-flow-col grid-cols-1 md:grid-cols-3 grid-rows-5 md:grid-rows-2 mr-0 md:mr-auto">
-          <div className="w-max">FAQs</div>
-          <div className="w-max">Contact Us</div>
-          <div className="w-max">Privacy Policy</div>
-          <div className="w-max">Press Kit</div>
-          <div className="w-max">Install Guide</div>
+        <div className="flex flex-col sm:grid gap-x-[4.8rem] items-center text-left gap-y-5 sm:gap-y-4 grid-flow-col grid-rows-3 lg:grid-rows-2 mr-0 md:mr-auto flex-shrink">
+          <div className="w-max cursor-pointer hover:border-b-darkGrayishBlue/80 border-b-transparent border-b transition-all">
+            FAQs
+          </div>
+          <div className="w-max cursor-pointer hover:border-b-darkGrayishBlue/80 border-b-transparent border-b transition-all">
+            Contact Us
+          </div>
+          <div className="w-max cursor-pointer hover:border-b-darkGrayishBlue/80 border-b-transparent border-b transition-all">
+            Privacy Policy
+          </div>
+          <div className="w-max cursor-pointer hover:border-b-darkGrayishBlue/80 border-b-transparent border-b transition-all">
+            Press Kit
+          </div>
+          <div className="w-max cursor-pointer hover:border-b-darkGrayishBlue/80 border-b-transparent border-b transition-all">
+            Install Guide
+          </div>
         </div>
-        <div className="flex flex-row justify-center items-center gap-6">
-          <div className="relative w-6 h-6">
+        <div className="flex flex-row justify-center items-center gap-10 sm:gap-6">
+          <div className="relative w-6 h-6 cursor-pointer">
             <Image src={fbIcon} fill alt="" />
           </div>
-          <div className="relative w-6 h-5">
+          <div className="relative w-6 h-5 cursor-pointer">
             <Image src={xIcon} fill alt="" />
           </div>
-          <div className="relative w-6 h-6">
+          <div className="relative w-6 h-6 cursor-pointer">
             <Image src={igIcon} fill alt="" />
           </div>
         </div>
