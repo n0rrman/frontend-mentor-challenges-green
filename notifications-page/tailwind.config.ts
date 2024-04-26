160462659abfacbc6a5d271f033a0913c64340c5
n0rrman <email@henriknorrman.com>
2024-04-26 23:59:08 +0700

    notifications-page fonts and colours added

--- notifications-page/tailwind.config.ts
+++ notifications-page/tailwind.config.ts
@@ -6,7 +6,22 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        primaryRed: 'hsl(var(--color-primaryRed) / <alpha-value>)',
+        primaryBlue: 'hsl(var(--color-primaryBlue) / <alpha-value>)',
+
+        white: 'hsl(var(--color-white) / <alpha-value>)',
+        veryLightGrayishBlue: 'hsl(var(--color-veryLightGrayishBlue) / <alpha-value>)',
+        lightGrayishBlue1: 'hsl(var(--color-lightGrayishBlue1) / <alpha-value>)',
+        lightGrayishBlue2: 'hsl(var(--color-lightGrayishBlue2) / <alpha-value>)',
+        grayishBlue: 'hsl(var(--color-grayishBlue) / <alpha-value>)',
+        darkGrayishBlue: 'hsl(var(--color-darkGrayishBlue) / <alpha-value>)',
+        veryDarkBlue: 'hsl(var(--color-veryDarkBlue) / <alpha-value>)',
+      }
+    }
+  },
   plugins: [],
 }
 export default config
