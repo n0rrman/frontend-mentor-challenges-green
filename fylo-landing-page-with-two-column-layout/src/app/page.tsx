215fc78eae902cc61109df5996e78de9b8226607
n0rrman <email@henriknorrman.com>
2024-04-27 19:49:22 +0700

    fylo-landing-page-with-two-column-layout desktop layout done

--- fylo-landing-page-with-two-column-layout/src/app/page.tsx
+++ fylo-landing-page-with-two-column-layout/src/app/page.tsx
@@ -1,8 +1,15 @@
 import Image from "next/image";
 
+import { FaInstagram, FaFacebookF, FaTwitter } from "react-icons/fa6";
+
 import { raleway } from "./fonts";
 import logo from "/public/logo.svg";
+import arrowIcon from "/public/icon-arrow.svg";
+import quotesIcon from "/public/icon-quotes.svg";
+import emailIcon from "/public/icon-email.svg";
+import phoneIcon from "/public/icon-phone.svg";
 import heroImg from "/public/illustration-1.svg";
+import profileImg from "/public/avatar-testimonial.jpg";
 import sectionImg from "/public/illustration-2.svg";
 import dividerDesktop from "/public/bg-curve-desktop.svg";
 import EmailForm from "@/components/email-form";
@@ -11,12 +18,12 @@ export default function Home() {
   return (
     <>
       <header
-        className={`flex flex-row justify-between p-6 sm:py-[4.55rem] sm:px-[5rem] ${raleway.className}`}
+        className={`flex flex-row justify-between p-6 sm:py-[4.05rem] sm:px-[5rem] ${raleway.className}`}
       >
         <div className="relative w-[10.375rem] aspect-[3.4] cursor-pointer">
           <Image fill src={logo} alt="" />
         </div>
-        <nav className="flex flex-row gap-[3.6rem] justify-center items-center text-[1rem]">
+        <nav className="flex flex-row gap-[3.58rem] justify-center items-center text-[1.01rem]">
           <div className="cursor-pointer border-b border-transparent hover:border-black transition-all">
             Features
           </div>
@@ -29,12 +36,12 @@ export default function Home() {
         </nav>
       </header>
       <main>
-        <section className="grid grid-cols-2 px-16 py-20">
-          <div className="flex flex-col gap-4">
-            <h1 className="text-4xl font-bold">
+        <section className="grid grid-cols-[0.48fr,0.52fr] justify-center gap-x-12 items-center px-20 pt-[1.4rem] pb-[10.2rem]">
+          <div className="flex flex-col gap-5 flex-0 -mt-7">
+            <h1 className={`${raleway.className} text-[2.5rem] font-bold`}>
               All your files in one secure location, accessible anywhere.
             </h1>
-            <p className="text-lg">
+            <p className="text-[1.125rem] leading-[1.4rem]">
               Fylo stores your most important files in one secure location.
               Access them wherever you need, share and collaborate with friends,
               family, and co-workers.
@@ -52,84 +59,134 @@ export default function Home() {
           <Image fill alt="" src={dividerDesktop} />
         </div>
 
-        <section className="grid grid-cols-2 bg-neutralBlue p-20">
-          <div className="flex flex-col gap-4">
-            <h1 className="font-bold text-3xl">
+        <section className="grid grid-cols-[0.55fr,0.45fr] bg-neutralBlue gap-[4rem] px-20 pt-14 pb-[6.55rem]">
+          <div className="flex flex-col gap-7">
+            <h1 className={`${raleway.className} font-bold text-[2.55rem]`}>
               Stay productive, wherever you are
             </h1>
 
-            <p>
-              Never let location be an issue when accessing your files. Fylo has
-              you covered for all of your file storage needs.
-            </p>
-            <p>
-              Securely share files and folders with friends, family and
-              colleagues for live collaboration. No email attachments required!
-            </p>
+            <div className="space-y-4 max-w-[57ch]">
+              <p>
+                Never let location be an issue when accessing your files. Fylo
+                has you covered for all of your file storage needs.
+              </p>
+              <p>
+                Securely share files and folders with friends, family and
+                colleagues for live collaboration. No email attachments
+                required!
+              </p>
 
-            <div className="text-accentCyan border-b border-accentCyan w-fit">
-              See how Fylo works
+              <div>
+                <div className="flex flex-row items-center gap-2 text-accentCyan border-b border-accentCyan w-fit pb-1  ">
+                  See how Fylo works
+                  <Image
+                    className="translate-y-[2px]"
+                    alt=""
+                    src={arrowIcon}
+                    width={16}
+                    height={16}
+                  />
+                </div>
+              </div>
             </div>
 
-            <div className="bg-white p-4 w-1/2 shadow-lg rounded text-sm">
-              <div>
+            <div className="bg-white mt-5 px-8 py-6 w-[22.2rem] shadow-lg rounded text-[0.87rem] leading-[1.45rem]">
+              <div className="relative mx-0.5 w-[0.825rem] h-[1.35rem]">
+                <Image className="pb-2" src={quotesIcon} fill alt="" />
+              </div>
+              <div className="py-0.5">
                 Fylo has improved our team productivity by an order of
                 magnitude. Since making the switch our team has become a
                 well-oiled collaboration machine.
               </div>
 
-              <div>
-                <div>Kyle Burton</div>
-                <div>Founder & CEO, Huddle</div>
+              <div className="flex flex-row items-center justify-start gap-2 pt-3">
+                <div className="relative h-8 w-8 rounded-full overflow-hidden">
+                  <Image alt="" fill src={profileImg} />
+                </div>
+                <div className="flex flex-col items-start justify-center -space-y-1.5 pt-1 ">
+                  <div className={`${raleway.className} font-bold text-xs`}>
+                    Kyle Burton
+                  </div>
+                  <div className="text-[0.55rem]">Founder & CEO, Huddle</div>
+                </div>
               </div>
             </div>
           </div>
           <div>
-            <div className="relative w-full aspect-[1.35]">
+            <div className="relative w-full aspect-[1.35] mt-8">
               <Image fill alt="" src={sectionImg} />
             </div>
           </div>
         </section>
 
-        <section className="bg-primaryDesaturatedBlue text-neutralBlue grid grid-cols-2 px-20 py-32">
-          <div>
-            <h1 className="text-3xl font-bold">Get early access today</h1>
-            <p>
+        <section className="bg-primaryDesaturatedBlue text-neutralBlue grid grid-cols-2 px-20 pt-[6.3rem] pb-[5.35rem]">
+          <div className="flex flex-col gap-4">
+            <h1 className={`${raleway.className} text-[2rem] font-bold`}>
+              Get early access today
+            </h1>
+            <p className="max-w-[58ch] text-neutralBlue/80 tracking-wide">
               It only takes a minute to sign up and our free starter tier is
               extremely generous. If you have any questions, our support team
               would be happy to help you.
             </p>
           </div>
-          <div>
+          <div className="pl-[18.5%] pr-[6%]">
             <EmailForm
               placeholder="email@example.com"
               buttonText="Get Started For Free"
+              vertical
             />
           </div>
         </section>
 
-        <footer className="bg-primaryDarkBlue text-neutralBlue p-20">
-          <div className="relative w-[10.375rem] h-[3.0625rem] cursor-pointer">
-            <Image className="text-white bg-clip-text" fill src={logo} alt="" />
+        <footer className="bg-primaryDarkBlue text-neutralBlue/80 px-20 pt-[6.75rem] pb-20">
+          <div className="relative w-[11rem] h-[3.25rem] cursor-pointer">
+            <Image
+              className="mix-blend-luminosity invert brightness-0"
+              fill
+              src={logo}
+              alt=""
+            />
           </div>
-          <div className="grid grid-cols-4">
-            <div>
-              <div>Phone: +1-543-123-4567</div>
-              <div>example@fylo.com</div>
+          <div className="grid grid-cols-4 py-6 px-0.5">
+            <div className="flex flex-col gap-4">
+              <div className="flex flex-row justify-start items-center gap-[1.2rem]">
+                <div className="relative w-[1.1rem] h-[1.1rem]">
+                  <Image src={phoneIcon} fill alt="" />
+                </div>
+                <div>Phone: +1-543-123-4567</div>
+              </div>
+              <div className="flex flex-row justify-start items-center gap-[1.2rem]">
+                <div className="relative w-5 h-4">
+                  <Image src={emailIcon} fill alt="" />
+                </div>
+                <div>example@fylo.com</div>
+              </div>
             </div>
-            <div className="mx-auto">
+            <div className="justify-self-center flex flex-col gap-3.5">
               <div>About Us</div>
               <div>Jobs</div>
               <div>Press</div>
               <div>Blog</div>
             </div>
 
-            <div className="mx-auto">
+            <div className="justify-self-center -ml-[5rem] flex flex-col gap-3.5">
               <div>Contact Us</div>
               <div>Terms</div>
               <div>Privacy</div>
             </div>
-            <div className="flex flex-row">F B I</div>
+            <div className="flex flex-row items-start gap-3.5 px-5 py-1">
+              <div className="border p-1.5 border-neutralBlue rounded-full text-md hover:cursor-pointer">
+                <FaFacebookF />
+              </div>
+              <div className="border p-1.5 border-neutralBlue rounded-full text-md hover:cursor-pointer">
+                <FaTwitter />
+              </div>
+              <div className="border p-1.5 border-neutralBlue rounded-full text-md hover:cursor-pointer">
+                <FaInstagram />
+              </div>
+            </div>
           </div>
         </footer>
       </main>
--- fylo-landing-page-with-two-column-layout/src/components/email-form.tsx
+++ fylo-landing-page-with-two-column-layout/src/components/email-form.tsx
@@ -6,30 +6,51 @@ import { useFormState } from "react-dom";
 interface EmailFormProps {
   buttonText: string;
   placeholder: string;
+  vertical?: boolean;
 }
 
-export default function EmailForm({ buttonText, placeholder }: EmailFormProps) {
+export default function EmailForm({
+  buttonText,
+  placeholder,
+  vertical,
+}: EmailFormProps) {
   const [formState, action] = useFormState(submitEmailFormState, {
     errors: {},
   });
 
-  console.log(formState.errors);
   return (
-    <>
-      <form action={action} className="flex flex-row gap-6">
+    <form
+      action={action}
+      className={`${
+        vertical ? "flex-col" : "flex-row"
+      } flex gap-4 py-2 items-start justify-start`}
+    >
+      <div className="flex flex-col w-max">
         <input
           name="email"
-          className="border border-primaryDarkBlue w-72"
+          className={`${
+            !formState.errors.email
+              ? "border-primaryDarkBlue"
+              : "border-rose-600"
+          } border text-sm placeholder:text-lightGrayish rounded ${
+            vertical ? "w-[33.5vw]" : "w-[20rem]"
+          } px-5 py-3`}
           placeholder={placeholder}
         />
-        <button
-          type="submit"
-          className="bg-accentBlue text-neutralBlue py-2 px-4"
+        <div
+          className={`${
+            !formState.errors.email ? "opacity-0" : "opacity-100"
+          } text-rose-600 text-[0.8rem] h-[1ch]`}
         >
-          {buttonText}
-        </button>
-      </form>
-      {!formState.success && <div>{formState.errors.email?.at(0)}</div>}
-    </>
+          {formState.errors.email?.at(0)}
+        </div>
+      </div>
+      <button
+        type="submit"
+        className="bg-accentBlue text-sm font-bold rounded text-neutralBlue text-nowrap px-16 py-[0.825rem]"
+      >
+        {buttonText}
+      </button>
+    </form>
   );
 }
