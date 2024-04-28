b7460853a22ecdb76cccc39f9596ab5bf50357da
n0rrman <email@henriknorrman.com>
2024-04-28 14:52:19 +0700

    fylo-landing-page-with-two-column-layout finished

--- fylo-landing-page-with-two-column-layout/src/app/page.tsx
+++ fylo-landing-page-with-two-column-layout/src/app/page.tsx
@@ -20,10 +20,10 @@ export default function Home() {
       <header
         className={`flex flex-row justify-between p-6 sm:py-[4.05rem] sm:px-[5rem] ${raleway.className}`}
       >
-        <div className="relative w-[10.375rem] aspect-[3.4] cursor-pointer">
+        <div className="relative w-[5rem] sm:w-[10.375rem] aspect-[3.4] cursor-pointer">
           <Image fill src={logo} alt="" />
         </div>
-        <nav className="flex flex-row gap-[3.58rem] justify-center items-center text-[1.01rem]">
+        <nav className="flex flex-row gap-6 sm:gap-[3.58rem] justify-center items-center text-xs sm:text-[1.01rem]">
           <div className="cursor-pointer border-b border-transparent hover:border-black transition-all">
             Features
           </div>
@@ -36,37 +36,43 @@ export default function Home() {
         </nav>
       </header>
       <main>
-        <section className="grid grid-cols-[0.48fr,0.52fr] justify-center gap-x-12 items-center px-20 pt-[1.4rem] pb-[10.2rem]">
-          <div className="flex flex-col gap-5 flex-0 -mt-7">
-            <h1 className={`${raleway.className} text-[2.5rem] font-bold`}>
+        <section className="grid grid-cols-1 lg:grid-cols-[0.48fr,0.52fr] justify-center gap-x-12 items-center px-9 sm:px-20 pt-[1.4rem] pb-[4rem] sm:pb-[10.2rem]">
+          <div className="flex flex-col gap-5 flex-0 mt-0 sm:-mt-7">
+            <h1
+              className={`${raleway.className} mt-10 text-[1.5rem] sm:text-[2.5rem] font-bold text-center sm:text-left`}
+            >
               All your files in one secure location, accessible anywhere.
             </h1>
-            <p className="text-[1.125rem] leading-[1.4rem]">
+            <p className="text-sm sm:text-[1.125rem] leading-[1.4rem] text-center sm:text-left">
               Fylo stores your most important files in one secure location.
               Access them wherever you need, share and collaborate with friends,
               family, and co-workers.
             </p>
-            <EmailForm
-              placeholder="Enter your email..."
-              buttonText="Get Started"
-            />
+            <div className="">
+              <EmailForm
+                placeholder="Enter your email..."
+                buttonText="Get Started"
+              />
+            </div>
           </div>
-          <div className="relative w-full aspect-[1.35]">
+          <div className="relative w-full order-first sm:order-last aspect-[1.35] mt-[3.25rem] sm:mt-0 ">
             <Image src={heroImg} alt="" fill />
           </div>
         </section>
-        <div className="relative w-full h-[7.125rem] pointer-events-none select-none">
+        <div className="relative w-full h-[3.5rem] sm:h-[7.125rem] pointer-events-none select-none">
           <Image fill alt="" src={dividerDesktop} />
         </div>
 
-        <section className="grid grid-cols-[0.55fr,0.45fr] bg-neutralBlue gap-[4rem] px-20 pt-14 pb-[6.55rem]">
+        <section className="grid grid-cols-1 lg:grid-cols-[0.55fr,0.45fr] bg-neutralBlue gap-[4rem] px-7 sm:px-20 pt-0 sm:pt-14 pb-[6.55rem]">
           <div className="flex flex-col gap-7">
-            <h1 className={`${raleway.className} font-bold text-[2.55rem]`}>
+            <h1
+              className={`${raleway.className} font-bold text-md text-center sm:text-left sm:text-[2.55rem] py-5`}
+            >
               Stay productive, wherever you are
             </h1>
 
-            <div className="space-y-4 max-w-[57ch]">
-              <p>
+            <div className="space-y-4 sm:max-w-[57ch] text-sm sm:text-inherit">
+              <p className="">
                 Never let location be an issue when accessing your files. Fylo
                 has you covered for all of your file storage needs.
               </p>
@@ -77,7 +83,7 @@ export default function Home() {
               </p>
 
               <div>
-                <div className="flex flex-row items-center gap-2 text-accentCyan border-b border-accentCyan w-fit pb-1  ">
+                <div className="flex flex-row items-center gap-2 text-accentCyan border-b border-accentCyan w-fit pb-1">
                   See how Fylo works
                   <Image
                     className="translate-y-[2px]"
@@ -90,7 +96,7 @@ export default function Home() {
               </div>
             </div>
 
-            <div className="bg-white mt-5 px-8 py-6 w-[22.2rem] shadow-lg rounded text-[0.87rem] leading-[1.45rem]">
+            <div className="bg-white mt-4 px-8 py-6 w-full sm:w-[22.2rem] shadow-lg rounded text-[0.87rem] leading-[1.45rem]">
               <div className="relative mx-0.5 w-[0.825rem] h-[1.35rem]">
                 <Image className="pb-2" src={quotesIcon} fill alt="" />
               </div>
@@ -113,25 +119,27 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div>
-            <div className="relative w-full aspect-[1.35] mt-8">
+          <div className="order-first sm:order-last">
+            <div className="relative w-full aspect-[1.30] mt-8">
               <Image fill alt="" src={sectionImg} />
             </div>
           </div>
         </section>
 
-        <section className="bg-primaryDesaturatedBlue text-neutralBlue grid grid-cols-2 px-20 pt-[6.3rem] pb-[5.35rem]">
+        <section className="bg-primaryDesaturatedBlue text-neutralBlue grid grid-cols-1 sm:grid-cols-2 px-12 sm:px-20 pt-12 sm:pt-[6.3rem] pb-10 sm:pb-[5.35rem]">
           <div className="flex flex-col gap-4">
-            <h1 className={`${raleway.className} text-[2rem] font-bold`}>
+            <h1
+              className={`${raleway.className} text-center sm:text-left text-lg sm:text-[2rem] font-bold`}
+            >
               Get early access today
             </h1>
-            <p className="max-w-[58ch] text-neutralBlue/80 tracking-wide">
+            <p className="max-w-[58ch] text-neutralBlue/80 tracking-wide text-sm sm:text-inherit text-center sm:text-left">
               It only takes a minute to sign up and our free starter tier is
               extremely generous. If you have any questions, our support team
               would be happy to help you.
             </p>
           </div>
-          <div className="pl-[18.5%] pr-[6%]">
+          <div className="px-0 sm:pl-[18.5%] sm:pr-[6%]">
             <EmailForm
               placeholder="email@example.com"
               buttonText="Get Started For Free"
@@ -140,7 +148,7 @@ export default function Home() {
           </div>
         </section>
 
-        <footer className="bg-primaryDarkBlue text-neutralBlue/80 px-20 pt-[6.75rem] pb-20">
+        <footer className="bg-primaryDarkBlue text-neutralBlue/80 px-10 sm:px-20 pt-[6.75rem] pb-[2.85rem]">
           <div className="relative w-[11rem] h-[3.25rem] cursor-pointer">
             <Image
               className="mix-blend-luminosity invert brightness-0"
@@ -149,8 +157,8 @@ export default function Home() {
               alt=""
             />
           </div>
-          <div className="grid grid-cols-4 py-6 px-0.5">
-            <div className="flex flex-col gap-4">
+          <div className="grid grid-cols-1 gap-10 md:gap-0 md:grid-cols-4 py-6 px-0.5">
+            <div className="flex flex-col md:gap-4">
               <div className="flex flex-row justify-start items-center gap-[1.2rem]">
                 <div className="relative w-[1.1rem] h-[1.1rem]">
                   <Image src={phoneIcon} fill alt="" />
@@ -164,19 +172,19 @@ export default function Home() {
                 <div>example@fylo.com</div>
               </div>
             </div>
-            <div className="justify-self-center flex flex-col gap-3.5">
+            <div className="justify-self-start md:justify-self-center flex flex-col gap-3.5">
               <div>About Us</div>
               <div>Jobs</div>
               <div>Press</div>
               <div>Blog</div>
             </div>
 
-            <div className="justify-self-center -ml-[5rem] flex flex-col gap-3.5">
+            <div className="justify-self-start md:justify-self-center ml-0 md:-ml-[5rem] flex flex-col gap-3.5">
               <div>Contact Us</div>
               <div>Terms</div>
               <div>Privacy</div>
             </div>
-            <div className="flex flex-row items-start gap-3.5 px-5 py-1">
+            <div className="flex flex-row justify-center items-start gap-3.5 px-5 py-1 flex-1">
               <div className="border p-1.5 border-neutralBlue rounded-full text-md hover:cursor-pointer">
                 <FaFacebookF />
               </div>
--- fylo-landing-page-with-two-column-layout/src/components/email-form.tsx
+++ fylo-landing-page-with-two-column-layout/src/components/email-form.tsx
@@ -22,10 +22,10 @@ export default function EmailForm({
     <form
       action={action}
       className={`${
-        vertical ? "flex-col" : "flex-row"
-      } flex gap-4 py-2 items-start justify-start`}
+        vertical ? "sm:flex-col" : "sm:flex-row"
+      } flex-col flex gap-4 py-2 items-start justify-start`}
     >
-      <div className="flex flex-col w-max">
+      <div className="flex flex-col w-full sm:w-max">
         <input
           name="email"
           className={`${
@@ -33,8 +33,8 @@ export default function EmailForm({
               ? "border-primaryDarkBlue"
               : "border-rose-600"
           } border text-sm placeholder:text-lightGrayish rounded ${
-            vertical ? "w-[33.5vw]" : "w-[20rem]"
-          } px-5 py-3`}
+            vertical ? "sm:w-[33.5vw]" : "sm:w-[20rem]"
+          } px-5 py-3 w-full`}
           placeholder={placeholder}
         />
         <div
@@ -47,7 +47,9 @@ export default function EmailForm({
       </div>
       <button
         type="submit"
-        className="bg-accentBlue text-sm font-bold rounded text-neutralBlue text-nowrap px-16 py-[0.825rem]"
+        className={`bg-accentBlue text-sm font-bold rounded text-neutralBlue shadow-lg text-nowrap w-full sm:w-auto ${
+          vertical ? "px-[2.1rem]" : "px-[3.95rem]"
+        } py-[0.825rem]`}
       >
         {buttonText}
       </button>
