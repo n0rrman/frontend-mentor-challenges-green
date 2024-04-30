dbea7859e5e5f1f246dcdef4607c97f56b08c7a8
n0rrman <email@henriknorrman.com>
2024-04-30 22:59:17 +0700

    notifications-page text content added

--- notifications-page/src/app/page.tsx
+++ notifications-page/src/app/page.tsx
@@ -1,5 +1,57 @@
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <main>
+      <div>
+        <h1>Notifications 3</h1>
+        <div>Mark all as read</div>
+      </div>
+
+      <div>
+        <div>
+          <div>
+            Mark Webber reacted to your recent post My first tournament today!
+          </div>
+          <div>1m ago</div>
+        </div>
+
+        <div>
+          <div>Angela Gray followed you</div>
+          <div>5m ago</div>
+        </div>
+
+        <div>
+          <div>Jacob Thompson has joined your group Chess Club</div>
+          <div>1 day ago</div>
+        </div>
+        <div>
+          <div>Rizky Hasanuddin sent you a private message</div>
+          <div>5 days ago</div>
+          <div>
+            Hello, thanks for setting up the Chess Club. I've been a member for
+            a few weeks now and I'm already having lots of fun and improving my
+            game.
+          </div>
+        </div>
+
+        <div>
+          <div>Kimberly Smith commented on your picture</div>
+          <div>1 week ago</div>
+        </div>
+
+        <div>
+          <div>
+            Nathan Peterson reacted to your recent post 5 end-game strategies to
+            increase your win rate
+          </div>
+          <div>2 weeks ago</div>
+        </div>
+        <div>
+          <div>Anna Kim left the group Chess Club</div>
+          <div>2 weeks ago</div>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 //   - Distinguish between "unread" and "read" notifications
