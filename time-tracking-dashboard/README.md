4946250ba03cfc0876ff89823d0d80b005949d68
n0rrman <email@henriknorrman.com>
2024-05-03 21:44:20 +0700

    time-tracking-dashboard init

---
+++ time-tracking-dashboard/README.md
@@ -0,0 +1,20 @@
+# Frontend Mentor - Time tracking dashboard solution
+
+This is a solution to the [Time tracking dashboard challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/time-tracking-dashboard-UIQ7167Jw). 
+
+
+### Screenshot
+
+![Screenshot of the completed challenge](./design/screenshot.png)
+
+
+### Links
+
+- Solution URL: [Solution at Frontend Mentor](...)
+- Live Site URL: [Live version on Vercel](...)
+
+
+### Built with
+
+- [Next.js](https://nextjs.org/) - JavaScript framework
+- [Tailwind CSS](https://tailwindcss.com/) - CSS framework
---
+++ time-tracking-dashboard/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Time tracking dashboard",
+  description:
+    "A perfect opportunity to practice your CSS Grid skills. For anyone wanting to take it up a notch, we provide a JSON data file to practice working with data.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
