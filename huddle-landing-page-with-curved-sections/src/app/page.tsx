b5aadc549238790030a9b1f888e488d33c8c222e
n0rrman <email@henriknorrman.com>
2024-05-16 18:51:11 +0700

    huddle-landing-page-with-curved-sections desktop hero section done

--- huddle-landing-page-with-curved-sections/src/app/page.tsx
+++ huddle-landing-page-with-curved-sections/src/app/page.tsx
@@ -17,60 +17,64 @@ import usersImg from "/public/illustration-your-users.svg";
 export default function Home() {
   return (
     <main className={openSans.className}>
-      <header className="flex flex-row justify-between items-start z-30 py-[3rem] px-20">
-        <div className="relative w-[12.5rem] h-8 aspect-[6.45] my-2">
+      <header className="flex flex-row justify-between items-start z-30 py-[4.5rem] px-16">
+        <div className="relative w-[15rem] min-h-8 aspect-[6.45] my-2 mx-4">
           <Image src={logo} alt="" fill />
         </div>
-        <button className="bg-white px-16 py-3.5 my-0 text-sm text-pink border-pink rounded-[2rem] shadow-grayishBlue/10 font-bold border-2">
+        <button
+          className={`${openSans.className} bg-white px-[1.6rem] py-2 my-1 text-[0.975rem] text-pink border-pink rounded-[2rem] font-bold tracking-snug shadow-grayishBlue/10 border-2 opacity-60`}
+        >
           Try It Free
         </button>
       </header>
 
-      <section className="flex flex-col justify-center items-center text-center gap-12 py-48">
+      <section className="flex flex-col justify-center items-center text-center gap-12 pt-[9.5rem] pb-[9rem]">
         <div className="flex flex-col gap-6 items-center">
-          <h1 className={`${poppins.className} text-4xl`}>
+          <h1 className={`${poppins.className} text-[3rem]`}>
             Build The Community Your Fans Will Love
           </h1>
-          <div className="w-[52ch] text-lg">
+          <div className="w-[52ch] text-xl leading-[1.8rem]">
             Huddle re-imagines the way we build communities. You have a voice,
             but so does your audience. Create connections with your users as you
             engage in genuine discussion.
           </div>
 
-          <button className="rounded-[2rem] py-4 px-12 text-white bg-pink text-xl">
+          <button className="rounded-[3rem] py-[1.6rem] px-[6.2rem] mt-6 text-white bg-pink text-xl font-bold shadow-xl hover:opacity-60 transition-all">
             Get Started For Free
           </button>
         </div>
 
-        <div className="min-w-[65rem] aspect-[1.4] min-h-[20rem] relative my-20">
+        <div className="min-w-[64.75rem] aspect-[1.4] min-h-[20rem] relative my-16">
           <Image fill alt="" src={heroImg} />
         </div>
 
-        <div className="flex flex-row justify-center gap-64 w-full text-xl">
-          <div className="flex flex-col gap-14">
-            <div className="w-10 h-10 relative">
-              <Image src={communityIcon} fill alt="" />
+        <div className="flex flex-row gap-[21.5rem] justify-center w-full text-2xl">
+          <div className="flex flex-col gap-14 items-start">
+            <div className="w-14 h-8">
+              <Image src={communityIcon} width={47} height={41} alt="" />
             </div>
             <div className="text-[6rem] font-bold">1.4k+</div>
-            <div className="opacity-50">Communities Formed</div>
-          </div>{" "}
-          <div className="flex flex-col gap-14">
-            <div className="w-10 h-10 relative">
-              <Image src={messageIcon} fill alt="" />
+            <div className="opacity-40 mx-auto">Communities Formed</div>
+          </div>
+          <div className="flex flex-col gap-14 items-start">
+            <div className="w-14 h-8">
+              <Image src={messageIcon} width={47} height={40} alt="" />
             </div>
             <div className="text-[6rem] font-bold">2.7m+</div>
-            <div className="opacity-50">Messages Sent</div>
+            <div className="opacity-40 mx-auto">Messages Sent</div>
           </div>
         </div>
       </section>
 
-      <div className="relative z-0 pointer-events-none select-none w-full h-32">
+      <div className="relative z-0 pointer-events-none select-none w-full h-40">
         <Image src={topSectionDivider1} alt="" className="object-fill" fill />
       </div>
       <section className="bg-veryPaleBlue ">
-        <div className="grid grid-cols-2">
-          <div>
-            <div>Grow Together</div>
+        <div className="grid grid-cols-2 px-[8.2rem] gap-[6.5rem] py-[4.6rem]">
+          <div className="w-[55ch] space-y-6 my-auto">
+            <div className={`${poppins.className} font-bold text-[2.5rem]`}>
+              Grow Together
+            </div>
 
             <div>
               Generate meaningful discussions with your audience and build a
@@ -78,13 +82,13 @@ export default function Home() {
               miss out on with a feedback form.
             </div>
           </div>
-          <div className="relative w-32 h-32">
+          <div className="relative w-[32rem] h-[26rem]">
             <Image fill src={growingImg} alt="" />
           </div>
         </div>
 
         <div className="bg-white">
-          <div className="relative z-0 pointer-events-none select-none w-full h-32">
+          <div className="relative z-0 pointer-events-none select-none w-full h-40">
             <Image
               src={bottomSectionDivider1}
               alt=""
@@ -93,12 +97,14 @@ export default function Home() {
             />
           </div>
 
-          <div className="grid grid-cols-2">
-            <div className="relative w-32 h-32">
+          <div className="grid grid-cols-2 px-[8.2rem] gap-[6.5rem] py-[4.6rem]">
+            <div className="relative w-[32rem] h-[26rem]">
               <Image fill src={flowingImg} alt="" />
             </div>
-            <div>
-              <div>Flowing Conversations</div>
+            <div className="w-[55ch] space-y-6 my-auto">
+              <div className={`${poppins.className} font-bold text-[2.5rem]`}>
+                Flowing Conversations
+              </div>
               <div>
                 You wouldn't paginate a conversation in real life, so why do it
                 online? Our threads have just-in-time loading for a more natural
@@ -106,7 +112,8 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div className="relative z-0 pointer-events-none select-none w-full h-32">
+
+          <div className="relative z-0 pointer-events-none select-none w-full h-40">
             <Image
               src={topSectionDivider2}
               alt=""
@@ -115,9 +122,12 @@ export default function Home() {
             />
           </div>
         </div>
-        <div className="grid grid-cols-2">
-          <div>
-            <div>Your Users</div>
+
+        <div className="grid grid-cols-2 px-[8.2rem] gap-[6.5rem] py-[4.6rem]">
+          <div className="w-[55ch] space-y-6 my-auto">
+            <div className={`${poppins.className} font-bold text-[2.5rem]`}>
+              Your Users
+            </div>
             <div>
               It takes no time at all to integrate Huddle with your app's
               authentication solution. This means, once signed in to your app,
@@ -125,12 +135,12 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="relative w-32 h-32">
+          <div className="relative w-[32rem] h-[26rem]">
             <Image fill src={usersImg} alt="" />
           </div>
         </div>
       </section>
-      <div className="relative z-0 pointer-events-none select-none w-full h-32">
+      <div className="relative z-0 pointer-events-none select-none w-full h-40">
         <Image
           src={bottomSectionDivider2}
           alt=""
@@ -139,31 +149,36 @@ export default function Home() {
         />
       </div>
 
-      <div className="flex flex-col justify-center items-center py-32 gap-6">
-        <h1 className={`${poppins.className} text-4xl`}>
+      <div className="flex flex-col justify-center items-center py-40 gap-2.5">
+        <h1 className={`${poppins.className} text-[2.5rem]`}>
           Ready To Build Your Community?
         </h1>
 
-        <button className="rounded-[2rem] py-4 px-12 text-white bg-pink text-xl">
+        <button className="rounded-[3rem] py-[1.6rem] px-[6.2rem] mt-6 text-white bg-pink text-xl font-bold shadow-xl hover:opacity-60 transition-all">
           Get Started For Free
         </button>
       </div>
 
-      <div className="relative z-0 pointer-events-none select-none w-full h-32">
+      <div className="relative z-0 pointer-events-none select-none w-full h-40">
         <Image src={footerSectionDivider} alt="" className="object-fill" fill />
       </div>
-      <footer className="grid grid-cols-2 bg-veryDarkCyan text-veryPaleBlue -mt-1">
+      <footer className="grid grid-cols-2 bg-veryDarkCyan text-veryPaleBlue -mt-1 px-32">
         <div>
-          <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-            nulla quam, hendrerit lacinia vestibulum a, ultrices quis sem.
+          <div className="relative w-[10rem] md:w-[15rem] h-6 md:h-9 invert brightness-0 my-10">
+            <Image src={logo} alt="" fill />
           </div>
+          <div>
+            <div>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              nulla quam, hendrerit lacinia vestibulum a, ultrices quis sem.
+            </div>
 
-          <div>Phone: +1-543-123-4567</div>
-          <div>example@huddle.com</div>
+            <div>Phone: +1-543-123-4567</div>
+            <div>example@huddle.com</div>
+          </div>
         </div>
         <div>
-          <div>Newsletter</div>
+          <div className="uppercase font-bold">Newsletter</div>
           <div>
             To recieve tips on how to grow your community, sign up to our weekly
             newsletter. We'll never send you spam or pass on your email address
