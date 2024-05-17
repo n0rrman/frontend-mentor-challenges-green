cfe4a25066927146930657602a42352da1568d86
n0rrman <email@henriknorrman.com>
2024-05-17 23:47:55 +0700

    time-tracking-dashboard colours added

--- time-tracking-dashboard/tailwind.config.ts
+++ time-tracking-dashboard/tailwind.config.ts
@@ -6,7 +6,26 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    extend:{
+      colors: {
+        blue: 'hsl(var(--color-blue) / <alpha-value>)',
+
+        work: 'hsl(var(--color-work) / <alpha-value>)',
+        play: 'hsl(var(--color-play) / <alpha-value>)',
+        study: 'hsl(var(--color-study) / <alpha-value>)',
+        exercise: 'hsl(var(--color-exercise) / <alpha-value>)',
+        social: 'hsl(var(--color-social) / <alpha-value>)',
+        selfCare: 'hsl(var(--color-selfCare) / <alpha-value>)',
+        
+        veryDarkBlue: 'hsl(var(--color-veryDarkBlue) / <alpha-value>)',
+        darkBlue: 'hsl(var(--color-darkBlue) / <alpha-value>)',
+        desaturatedBlue: 'hsl(var(--color-desaturatedBlue) / <alpha-value>)',
+        paleBlue: 'hsl(var(--color-paleBlue) / <alpha-value>)',
+
+      }
+    }
+  },
   plugins: [],
 }
 export default config
