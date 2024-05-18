95bc5f62c3f475646ddedbe33b37f36d3865fe55
n0rrman <email@henriknorrman.com>
2024-05-18 23:57:14 +0700

    time-tracking-dashboard text content added

--- time-tracking-dashboard/src/app/page.tsx
+++ time-tracking-dashboard/src/app/page.tsx
@@ -1,5 +1,52 @@
 export default function Home() {
-    return (
-      <div>Home Page</div>  
-    );
-  }
+  return (
+    <div>
+      <div>
+        <div>Report for</div>
+        <div>Jeremy Robson</div>
+        <div>Daily</div>
+        <div>Weekly</div>
+        <div>Monthly</div>
+      </div>
+
+      <div>
+        <div>Work</div>
+
+        <div>
+          <div>5hrs</div>
+          <div>Previous - 7hrs</div>
+        </div>
+      </div>
+
+      <div>
+        <div>Play</div>
+
+        <div>1hr</div>
+        <div>Previous - 2hrs</div>
+      </div>
+
+      <div>
+        <div>Study</div>
+        <div>0hrs</div>
+        <div>Previous - 1hr</div>
+      </div>
+      <div>
+        <div>Exercise</div>
+        <div>1hr</div>
+        <div>Previous - 1hr</div>
+      </div>
+
+      <div>
+        <div>Social</div>
+        <div>1hr</div>
+        <div>Previous - 23hrs</div>
+      </div>
+
+      <div>
+        <div>Self Care</div>
+        <div>0hrs</div>
+        <div>Previous - 1hr</div>
+      </div>
+    </div>
+  );
+}
