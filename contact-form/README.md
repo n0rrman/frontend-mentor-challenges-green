8d182cbe7958ea3fb0e789d680bfe8b317acc21c
n0rrman <email@henriknorrman.com>
2024-05-20 23:58:56 +0700

    contact-form init

---
+++ contact-form/README.md
@@ -0,0 +1,3 @@
+# Frontend Mentor - Contact form solution
+
+This is a solution to the [Contact form challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/contact-form--G-hYlqKJj). 
---
+++ contact-form/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Contact Form",
+  description:
+    "Building accessible forms is a crucial task for front-end developers. This challenge will help you practice building a form with several input types and validation.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
