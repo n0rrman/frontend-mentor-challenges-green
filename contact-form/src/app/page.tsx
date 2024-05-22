074d231ac27adcd3000c2978285c221ab9b862b1
n0rrman <email@henriknorrman.com>
2024-05-22 23:21:25 +0700

    contact-form text content added

--- contact-form/src/app/page.tsx
+++ contact-form/src/app/page.tsx
@@ -1,5 +1,25 @@
 export default function Home() {
-    return (
-      <div>Home Page</div>  
-    );
-  }
+  return (
+    <div>
+      <div>Contact Us</div>
+      <div>First Name</div>
+      <div>This field is required</div>
+      <div>Last Name</div>
+      <div>This field is required</div>
+      <div>Email Address</div>
+      <div>Please enter a valid email address</div>
+      <div>This field is required</div>
+      <div>Query Type</div>
+      <div>General Enquiry</div>
+      <div>Support Request</div>
+      <div>Please select a query type</div>
+      <div>Message</div>
+      <div>This field is required</div>
+      <div>I consent to being contacted by the team</div>
+      <div>To submit this form, please consent to being contacted</div>
+      <div>Submit</div>
+      <div>Message Sent!</div>
+      <div>Thanks for completing the form. We'll be in touch soon!</div>
+    </div>
+  );
+}
