30e6627859df177ab6f24fabf30e2ba0bdba6f20
n0rrman <email@henriknorrman.com>
2024-05-23 23:51:41 +0700

    contact-form colours added

--- contact-form/tailwind.config.ts
+++ contact-form/tailwind.config.ts
@@ -6,7 +6,19 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        lightGreen: 'hsl(var(--color-lightGreen) / <alpha-value>)',
+        green: 'hsl(var(--color-green) / <alpha-value>)',
+        red: 'hsl(var(--color-red) / <alpha-value>)',
+
+        white: 'hsl(var(--color-white) / <alpha-value>)',
+        mediumGrey: 'hsl(var(--color-mediumGrey) / <alpha-value>)',
+        darkGrey: 'hsl(var(--color-darkGrey) / <alpha-value>)',
+      }
+    }
+  },
   plugins: [],
 }
 export default config
