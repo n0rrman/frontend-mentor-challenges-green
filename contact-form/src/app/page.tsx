4f748dbfa93f0ef383627160151c790434b513d6
n0rrman <email@henriknorrman.com>
2024-05-24 23:51:29 +0700

    contact-form text content cleaned up

--- contact-form/src/app/page.tsx
+++ contact-form/src/app/page.tsx
@@ -2,24 +2,37 @@ export default function Home() {
   return (
     <div>
       <div>Contact Us</div>
-      <div>First Name</div>
-      <div>This field is required</div>
-      <div>Last Name</div>
-      <div>This field is required</div>
-      <div>Email Address</div>
-      <div>Please enter a valid email address</div>
-      <div>This field is required</div>
-      <div>Query Type</div>
-      <div>General Enquiry</div>
-      <div>Support Request</div>
-      <div>Please select a query type</div>
-      <div>Message</div>
-      <div>This field is required</div>
-      <div>I consent to being contacted by the team</div>
-      <div>To submit this form, please consent to being contacted</div>
-      <div>Submit</div>
-      <div>Message Sent!</div>
-      <div>Thanks for completing the form. We'll be in touch soon!</div>
+      <div>
+        <div>First Name</div>
+        {/* <div>This field is required</div> */}
+        <div>Last Name</div>
+        {/* <div>This field is required</div> */}
+      </div>
+      <div>
+        <div>Email Address</div>
+        {/* <div>Please enter a valid email address</div> */}
+        {/* <div>This field is required</div> */}
+      </div>
+      <div>
+        <div>Query Type</div>
+        <div>General Enquiry</div>
+        <div>Support Request</div>
+        {/* <div>Please select a query type</div> */}
+      </div>
+      <div>
+        <div>Message</div>
+        {/* <div>This field is required</div> */}
+      </div>
+      <div>
+        <div>I consent to being contacted by the team</div>
+        {/* <div>To submit this form, please consent to being contacted</div> */}
+      </div>
+
+      <div>
+        <div>Submit</div>
+        {/* <div>Message Sent!</div> */}
+        {/* <div>Thanks for completing the form. We'll be in touch soon!</div> */}
+      </div>
     </div>
   );
 }
