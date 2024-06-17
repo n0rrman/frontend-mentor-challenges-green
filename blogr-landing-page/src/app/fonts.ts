58d2102a1127a917d447d3e5668fc2da8d213fcb
n0rrman <email@henriknorrman.com>
2024-06-17 23:49:44 +0700

    blogr-landing-page fonts added

---
+++ blogr-landing-page/src/app/fonts.ts
@@ -0,0 +1,4 @@
+import { Overpass, Ubuntu } from "next/font/google";
+
+export const overpass = Overpass({ subsets: ["latin"], weight: ["300", "600"] });
+export const ubuntu = Ubuntu({ subsets: ["latin"], weight: ["400", "500", "700"], });
--- blogr-landing-page/src/app/layout.tsx
+++ blogr-landing-page/src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import { ubuntu } from "./fonts";
 
 export const metadata: Metadata = {
   title: "Blogr landing page",
@@ -17,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={ubuntu.className}>{children}</body>
     </html>
   );
 }
