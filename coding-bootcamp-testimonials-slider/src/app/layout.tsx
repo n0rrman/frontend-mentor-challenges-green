6dac2c3857c8168a8547c6de5e521654ddd3e580
n0rrman <email@henriknorrman.com>
2024-06-20 23:25:11 +0100

    coding-bootcamp-testimonials-slider init

---
+++ coding-bootcamp-testimonials-slider/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Coding bootcamp testimonials slider",
+  description:
+    "This challenge will be a nice test if you're new to JavaScript. It's also a great opportunity to play around with content animations and transitions.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
