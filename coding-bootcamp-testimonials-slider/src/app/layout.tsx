738a7492fb534b39aefc654cf710ab125975da0c
n0rrman <email@henriknorrman.com>
2024-06-21 22:51:50 +0100

    coding-bootcamp-testimonials-slider fonts added

--- coding-bootcamp-testimonials-slider/src/app/layout.tsx
+++ coding-bootcamp-testimonials-slider/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], weight: ["300", "500", "700"] });
 
 export const metadata: Metadata = {
   title: "Coding bootcamp testimonials slider",
