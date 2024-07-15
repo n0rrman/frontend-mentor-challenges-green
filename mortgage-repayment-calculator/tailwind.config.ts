1babce703625ab1d7f1dd1d3695643bac0e633e4
n0rrman <email@henriknorrman.com>
2024-07-15 22:42:15 +0200

    mortgage-repayment-calculator colours added

--- mortgage-repayment-calculator/tailwind.config.ts
+++ mortgage-repayment-calculator/tailwind.config.ts
@@ -6,7 +6,22 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        lime: 'hsl(var(--color-lime) / <alpha-value>)',
+        red: 'hsl(var(--color-red) / <alpha-value>)',
+        white: 'hsl(var(--color-white) / <alpha-value>)',
+        slate: {
+          100: 'hsl(var(--color-slate-100) / <alpha-value>)',
+          300: 'hsl(var(--color-slate-300) / <alpha-value>)',
+          500: 'hsl(var(--color-slate-500) / <alpha-value>)',
+          700: 'hsl(var(--color-slate-700) / <alpha-value>)',
+          900: 'hsl(var(--color-slate-900) / <alpha-value>)',
+        }
+      }
+    }
+  },
   plugins: [],
 }
 export default config
