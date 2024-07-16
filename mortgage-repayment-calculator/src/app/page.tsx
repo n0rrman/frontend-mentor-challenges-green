2f7e674af86ba116139a5757195d80e7de7639fb
n0rrman <email@henriknorrman.com>
2024-07-16 16:39:37 +0200

    mortgage-repayment-calculator text content added

--- mortgage-repayment-calculator/src/app/page.tsx
+++ mortgage-repayment-calculator/src/app/page.tsx
@@ -1,5 +1,39 @@
 export default function Home() {
-    return (
-      <div>Home Page</div>  
-    );
-  }
+  return (
+    <div>
+      <div>Mortgage Calculator</div>
+      <div>Clear All</div>
+
+      <div>Mortgage Amount</div>
+
+      <div>Mortgage Term</div>
+
+      <div>Interest Rate</div>
+
+      <div>Mortgage Type</div>
+      <div>Repayment</div>
+      <div>Interest Only</div>
+
+      <div>Calculate Repayments</div>
+
+      <div>Results shown here</div>
+
+      <div>
+        Complete the form and click “calculate repayments” to see what your
+        monthly repayments would be.
+      </div>
+
+      <div>Your results</div>
+
+      <div>
+        Your results are shown below based on the information you provided. To
+        adjust the results, edit the form and click “calculate repayments”
+        again.
+      </div>
+
+      <div>Your monthly repayments</div>
+
+      <div>Total you'll repay over the term</div>
+    </div>
+  );
+}
