d095acc357183d7a74a56baa3d9f2ac01b429bcf
n0rrman <email@henriknorrman.com>
2024-07-19 23:56:50 +0200

    interactive-pricing-component text content added

--- interactive-pricing-component/src/app/page.tsx
+++ interactive-pricing-component/src/app/page.tsx
@@ -1,5 +1,18 @@
 export default function Home() {
-    return (
-      <div>Home Page</div>  
-    );
-  }
+  return (
+    <div>
+      <div>Simple, traffic-based pricing</div>
+      <div>Sign-up for our 30-day trial. No credit card required.</div>
+      <div>Pageviews</div>
+      <div>$ /month</div>
+
+      <div>Monthly Billing</div>
+      <div>Yearly Billing 25% discount</div>
+      <div>Unlimited websites</div>
+      <div>100% data ownership</div>
+      <div>Email reports</div>
+
+      <div>Start my trial</div>
+    </div>
+  );
+}
