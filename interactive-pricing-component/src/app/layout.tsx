f882e8674d6c4abe7656430713c1453580bff524
n0rrman <email@henriknorrman.com>
2024-07-22 23:56:40 +0200

    interactive-pricing-component fonts added

--- interactive-pricing-component/src/app/layout.tsx
+++ interactive-pricing-component/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Manrope } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const manrope = Manrope({ subsets: ["latin"], weight: ["600", "800"] });
 
 export const metadata: Metadata = {
   title: "Interactive pricing component",
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={manrope.className}>{children}</body>
     </html>
   );
 }
--- interactive-pricing-component/tailwind.config.ts
+++ interactive-pricing-component/tailwind.config.ts
@@ -6,7 +6,22 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    colors: {
+      fullSlider: 'hsl(var(--color-fullSlider) / <alpha-value>)',
+      sliderBg: 'hsl(var(--color-sliderBg) / <alpha-value>)',
+      discountBg: 'hsl(var(--color-discountBg) / <alpha-value>)',
+      discountText: 'hsl(var(--color-discountText) / <alpha-value>)',
+      ctaText: 'hsl(var(--color-ctaText) / <alpha-value>)',
+      
+      pricingCompBg: 'hsl(var(--color-pricingCompBg) / <alpha-value>)',
+      mainBg: 'hsl(var(--color-mainBg) / <alpha-value>)',
+      emptySlider: 'hsl(var(--color-emptySlider) / <alpha-value>)',
+      toggleBg: 'hsl(var(--color-toggleBg) / <alpha-value>)',
+      text: 'hsl(var(--color-text) / <alpha-value>)',
+      ctaBg: 'hsl(var(--color-ctaBg) / <alpha-value>)',
+    }
+  },
   plugins: [],
 }
 export default config
