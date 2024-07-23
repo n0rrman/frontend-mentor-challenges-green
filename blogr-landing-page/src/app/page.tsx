fc8f0b1db49fa9f5bd52444c9e98532c09ab5e93
n0rrman <email@henriknorrman.com>
2024-07-23 23:52:23 +0200

    blogr-landing-page text content added

--- blogr-landing-page/src/app/page.tsx
+++ blogr-landing-page/src/app/page.tsx
@@ -1,5 +1,91 @@
 export default function Home() {
-    return (
-      <div>Home Page</div>  
-    );
-  }
+  return (
+    <div>
+      <div>Product</div>
+
+      <div>Overview</div>
+      <div>Pricing</div>
+      <div>Marketplace</div>
+      <div>Features</div>
+      <div>Integrations</div>
+
+      <div>Company</div>
+
+      <div>About</div>
+      <div>Team</div>
+      <div>Blog</div>
+      <div>Careers</div>
+
+      <div>Connect</div>
+      <div>Contact</div>
+      <div>Newsletter</div>
+      <div>LinkedIn</div>
+      <div>Login</div>
+      <div>Sign Up</div>
+      <div>A modern publishing platform</div>
+      <div>Grow your audience and build your online brand</div>
+      <div>Start for Free</div>
+      <div>Learn More</div>
+
+      <div>Designed for the future</div>
+      <div>Introducing an extensible editor</div>
+      <div>
+        Blogr features an exceedingly intuitive interface which lets you focus
+        on one thing: creating content.
+      </div>
+      <div>
+        The editor supports management of multiple blogs and allows easy
+        manipulation of embeds such as images, videos, and Markdown.
+        Extensibility with plugins and themes provide easy ways to add
+        functionality or change the looks of a blog.
+      </div>
+
+      <div>Robust content management</div>
+      <div>
+        Flexible content management enables users to easily move through posts.
+        Increase the usability of your blog by adding customized categories,
+        sections, format, or flow. With this functionality, you’re in full
+        control.
+      </div>
+
+      <div>State of the Art Infrastructure</div>
+      <div>
+        With reliability and speed in mind, worldwide data centers provide the
+        backbone for ultra-fast connectivity. This ensures your site will load
+        instantly, no matter where your readers are, keeping your site
+        competitive.
+      </div>
+
+      <div>Free, open, simple</div>
+      <div>
+        Blogr is a free and open source application backed by a large community
+        of helpful developers. It supports features such as code syntax
+        highlighting, RSS feeds, social media integration, third-party
+        commenting tools, and works seamlessly with Google Analytics. The
+        architecture is clean and is relatively easy to learn.
+      </div>
+
+      <div>Powerful tooling</div>
+      <div>
+        Batteries included. We built a simple and straightforward CLI tool that
+        makes customization and deployment a breeze, but capable of producing
+        even the most complicated sites.
+      </div>
+      <div>Product</div>
+      <div>Overview</div>
+      <div>Pricing</div>
+      <div>Marketplace</div>
+      <div>Features</div>
+      <div>Integrations</div>
+      <div>Company</div>
+      <div>About</div>
+      <div>Team</div>
+      <div>Blog</div>
+      <div>Careers</div>
+      <div>Connect</div>
+      <div>Contact</div>
+      <div>Newsletter</div>
+      <div>LinkedIn</div>
+    </div>
+  );
+}
