15e0ee1b0d5d43600b584aa31b80032b969fedfa
n0rrman <email@henriknorrman.com>
2024-07-24 23:49:48 +0200

    interactive-pricing-component semenatic html + initial structure

--- interactive-pricing-component/src/app/layout.tsx
+++ interactive-pricing-component/src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={manrope.className}>{children}</body>
+      <body className={`${manrope.className} bg-mainBg`}>{children}</body>
     </html>
   );
 }
--- interactive-pricing-component/src/app/page.tsx
+++ interactive-pricing-component/src/app/page.tsx
@@ -1,18 +1,31 @@
 export default function Home() {
   return (
-    <div>
-      <div>Simple, traffic-based pricing</div>
-      <div>Sign-up for our 30-day trial. No credit card required.</div>
-      <div>Pageviews</div>
-      <div>$ /month</div>
+    <main>
+      <div id="headings">
+        <h1>Simple, traffic-based pricing</h1>
+        <h2>Sign-up for our 30-day trial. No credit card required.</h2>
+      </div>
+      <div className="bg-pricingCompBg" id="card">
+        <div>
+          <div>Pageviews</div>
+          <div>$ /month</div>
+        </div>
 
-      <div>Monthly Billing</div>
-      <div>Yearly Billing 25% discount</div>
-      <div>Unlimited websites</div>
-      <div>100% data ownership</div>
-      <div>Email reports</div>
+        <div>
+          <div>Monthly Billing</div>
+          <div>Yearly Billing</div>
+          <div>25% discount</div>
+        </div>
+        <div>
+          <div>
+            <div>Unlimited websites</div>
+            <div>100% data ownership</div>
+            <div>Email reports</div>
+          </div>
 
-      <div>Start my trial</div>
-    </div>
+          <button>Start my trial</button>
+        </div>
+      </div>
+    </main>
   );
 }
